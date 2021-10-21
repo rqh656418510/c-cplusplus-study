0425d223730287bcb7fad22e584010ec121f377d
rqh656418510 <656418510@qq.com>
2021-10-21 17:56:32 +0800

    feat: add code files

---
+++ c++-study/day01/demo05/main2.cpp
@@ -0,0 +1,21 @@
+/**
+ * 1. C++ 对 C 语言的增强
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int a;
+
+int main() {
+
+    /************************ 作用域限定运算符 ******************************/
+
+    float a;
+    a = 3.14;
+    ::a = 6;
+    cout << "local variable a = " << a << endl;
+    cout << "global variable a = " << ::a << endl;
+    return 0;
+}
