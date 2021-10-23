167796d4d132bfa6b1c357de535ed5ed1e7c5b62
rqh656418510 <656418510@qq.com>
2021-10-23 11:56:14 +0800

    feat: add code files

---
+++ c++-study/day03/demo01/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * 1. 构造函数与析构函数的使用案例
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Teacher {
+
+public:
+
+    // 定义无参构造函数
+    Teacher() {
+        cout << "调用构造函数" << endl;
+    }
+
+    // 定义析构函数
+    ~Teacher() {
+        cout << "调用析构函数" << endl;
+    }
+
+};
+
+int main() {
+    Teacher teacher;
+    return 0;
+}
