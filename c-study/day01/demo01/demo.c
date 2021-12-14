b039455d93f91a3feaf611c7e3628913cbf96203
rqh656418510 <656418510@qq.com>
2021-12-14 21:54:53 +0800

    feat: add code files

---
+++ c-study/day01/demo01/demo.c
@@ -0,0 +1,10 @@
+/**
+ * 1. 第一个 C 语言程序
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ c-study/day01/demo02/demo.c
@@ -0,0 +1,12 @@
+/**
+ * 1. system 函数
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    system("ls");      // Linux 平台
+    // system("calc");          // Windows 平台
+    return 0;
+}
