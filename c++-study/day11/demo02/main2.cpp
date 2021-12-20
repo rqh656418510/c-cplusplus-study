7c19e6c9883565049db4537c2115ce19266eb0b1
rqh656418510 <656418510@qq.com>
2021-12-21 01:27:12 +0800

    feat: add code files

--- c++-study/day11/demo02/main2.cpp
+++ c++-study/day11/demo02/main2.cpp
@@ -46,6 +46,39 @@ int main() {
     string str9 = "abc";
     int result = str8.compare(str9);    // 返回值小于等于-1
     cout << "result = " << result << endl;
-    
+
+    // 截取子字符串
+    string str10 = "124abc";
+    string str11 = str10.substr(1, 3);
+    cout << "str11 = " << str11 << endl;
+
+    // 字符串的区间插入
+    string str12 = "abcdef";
+    str12.insert(2, "123");
+    cout << "str12 = " << str12 << endl;
+
+    // 字符串的区间删除
+    string str13 = "123456";
+    str13.erase(2, 2);
+    cout << "str13 = " << str13 << endl;
+
+    // 从字符串取得 char *
+    string str14 = "hijkl";
+    const char *p1 = str14.c_str();
+    cout << "p1 = " << p1 << endl;
+
+    // char * 隐式类型转换为 string
+    char *p2 = "abc123";
+    string str15 = p2;
+    cout << "str15 = " << str15 << endl;
+
+    // 将 string 拷贝到 char* 指向的内存空间
+    char *p3 = new char[3];
+    string str16 = "hello jim";
+    int number = str16.copy(p3, 3, 2);
+    cout << "number = " << number << endl;
+    cout << "p3 = " << p3 << endl;
+    delete[] p3;
+
     return 0;
 }
