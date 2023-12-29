fb38c488b9e27714a6402480f66bbc5911740c56
rqh656418510 <656418510@qq.com>
2023-12-29 22:53:09 +0800

    feat: update code files

--- c++-study/day12/demo12/main.cpp
+++ c++-study/day12/demo12/main.cpp
@@ -1,8 +1,8 @@
 /*
- * 1. #### 常用函数适配器的使用
+ * 1. 绑定适配器的使用
  *
  * 第一步：绑定数据，如 bind2nd 或者 bind1st
- * 第二步：函数对象继承类 binary_function<参数类型１, 参数类型２, 返回值类型>
+ * 第二步：函数对象继承自 binary_function<参数类型１, 参数类型２, 返回值类型>
  * 第三步：operator() 加 const 修饰符
 */
 
@@ -24,8 +24,7 @@ public:
 
 };
 
-void test01() {
-    cout << "------ 绑定适配器（binder）的使用 ------" << endl;
+int main() {
 
     vector<int> v;
     for (int i = 0; i < 10; i++) {
@@ -36,39 +35,5 @@ void test01() {
     int num = 100;
     for_each(v.begin(), v.end(), bind2nd(MyPrint(), num));
 
-    cout << endl;
-}
-
-// 函数对象，继承自 unary_function
-class GreaterThenFive : public unary_function<int, bool> {
-
-public:
-
-    bool operator()(int v) const {
-        return v > 5;
-    }
-
-};
-
-void test02() {
-    cout << "------ 取反适配器（negator）的使用 ------" << endl;
-
-    vector<int> v;
-    for (int i = 0; i < 10; i++) {
-        v.push_back(i);
-    }
-
-    // 查找第一个小于５的数字
-    vector<int>::iterator pos = find_if(v.begin(), v.end(), not1(GreaterThenFive()));
-    if (pos != v.end()) {
-        cout << "found first number less then five is " << *pos << endl;
-    } else {
-        cout << "not found first number less then five" << endl;
-    }
-}
-
-int main() {
-    test01();
-    test02();
     return 0;
 }
---
+++ c++-study/day12/demo12/main2.cpp
@@ -0,0 +1,51 @@
+/*
+ * 1. 取反适配器的使用
+ *
+ * 第一步：取反，如 not1 或者 not2
+ * 第二步：函数对象继承自 unary_function<参数类型, 返回值类型>
+ * 第三步：operator() 加 const 修饰符
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+// 函数对象，继承自 unary_function
+class GreaterThenFive : public unary_function<int, bool> {
+
+public:
+
+    bool operator()(int v) const {
+        return v > 5;
+    }
+
+};
+
+int main() {
+
+    vector<int> v;
+    for (int i = 0; i < 10; i++) {
+        v.push_back(i);
+    }
+
+    // 查找第一个小于５的数字（第一种写法）
+    vector<int>::iterator pos = find_if(v.begin(), v.end(), not1(GreaterThenFive()));
+    if (pos != v.end()) {
+        cout << "found first number less then five is " << *pos << endl;
+    } else {
+        cout << "not found first number less then five" << endl;
+    }
+
+    // 查找第一个小于５的数字（第二种写法）
+    vector<int>::iterator pos2 = find_if(v.begin(), v.end(), not1(bind2nd(greater<int>(), 5)));
+    if (pos2 != v.end()) {
+        cout << "found first number less then five is " << *pos2 << endl;
+    } else {
+        cout << "not found first number less then five" << endl;
+    }
+
+    return 0;
+}
