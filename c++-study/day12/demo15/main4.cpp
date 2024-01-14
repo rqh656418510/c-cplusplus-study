16425ed3d4e0a345463cfe6bb5e2d59bacf27e44
rqh656418510 <656418510@qq.com>
2024-01-14 22:53:12 +0800

    feat: update code files

--- c++-study/day12/demo15/main4.cpp
+++ c++-study/day12/demo15/main4.cpp
@@ -1,7 +1,7 @@
 /*
  * 1. 常用的排序算法 - reverse
  *
- * <p> 反转指定范围的元素
+ * <p> 反转指定范围元素的位置
 */
 
 #include <iostream>
@@ -10,5 +10,22 @@
 using namespace std;
 
 int main() {
+
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(5);
+    v.push_back(8);
+    v.push_back(7);
+    v.push_back(9);
+
+    for_each(v.begin(), v.end(), [](int value) { cout << value << " "; });
+
+    cout << endl;
+
+    // 反转指定范围元素的位置
+    reverse(v.begin(), v.end());
+
+    for_each(v.begin(), v.end(), [](int value) { cout << value << " "; });
+
     return 0;
 }
