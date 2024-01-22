57379ce97bc3b623491b653634381b3c04348279
rqh656418510 <656418510@qq.com>
2024-01-22 14:29:05 +0800

    feat: update code files

--- c++-study/day12/demo17/main.cpp
+++ c++-study/day12/demo17/main.cpp
@@ -6,10 +6,21 @@
 
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <numeric>
 
 using namespace std;
 
 int main() {
+
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(2);
+
+    // 计算容器元素累计总和
+    int sum = accumulate(v.begin(), v.end(), 0);
+
+    cout << "sum = " << sum << endl;
+
     return 0;
 }
