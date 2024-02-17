141e45705f1e2bdb29f21771d7903f4bc35f0e20
rqh656418510 <656418510@qq.com>
2024-02-17 22:44:20 +0800

    feat: update code files

---
+++ c++-study-2/day01/demo03/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * 1. 命名空间的使用
+ */
+
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+int main() {
+
+    cout << ::global << endl;
+
+}
---
+++ c++-study-2/day01/demo03/main.h
@@ -0,0 +1,6 @@
+#ifndef C___STUDY_MAIN_H
+#define C___STUDY_MAIN_H
+
+int global = 200;
+
+#endif
