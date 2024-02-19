444a5921e6d36e7d6f096d7519a63dac78e8a274
rqh656418510 <656418510@qq.com>
2024-02-19 13:51:31 +0800

    feat: update code files

--- c++-study-2/day01/demo05/main.cpp
+++ c++-study-2/day01/demo05/main.cpp
@@ -6,6 +6,19 @@
 
 using namespace std;
 
+// 全局的常量，受到保护，不允许更改值
+const int m_A = 10;
+
+void test01() {
+    // 局部的常量，无法通过指针更改值
+    const int m_B = 20;
+    int *p = (int *) &m_B;
+    *p = 30;
+    cout << "*p = " << *p << endl;
+    cout << "m_B = " << m_B << endl;
+}
+
 int main() {
-    
+    test01();
+    return 0;
 }
