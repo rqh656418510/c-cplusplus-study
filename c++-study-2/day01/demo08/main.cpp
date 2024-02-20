2bf21e7c8aa0cbe623c4d7da8f278cce99d3ffa5
rqh656418510 <656418510@qq.com>
2024-02-20 13:01:06 +0800

    feat: update code files

--- c++-study-2/day01/demo08/main.cpp
+++ c++-study-2/day01/demo08/main.cpp
@@ -6,6 +6,36 @@
 
 using namespace std;
 
+// 指针作函数参数
+void mySwap1(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+// 引用作函数参数
+void mySwap2(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+// 使用指针交换两个变量的值
+void test01() {
+    int a = 10, b = 20;
+    mySwap1(&a, &b);
+    cout << "a = " << a << ", b = " << b << endl;
+}
+
+// 使用引用交换两个变量的值
+void test02() {
+    int a = 50, b = 60;
+    mySwap2(a, b);
+    cout << "a = " << a << ", b = " << b << endl;
+}
+
 int main() {
+    test01();
+    test02();
     return 0;
 }
