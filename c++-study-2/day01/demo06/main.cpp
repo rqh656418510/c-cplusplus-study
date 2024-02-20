2f2c00a6d6b7d7ff0705c3ceab8aa6a3f6d87fe3
rqh656418510 <656418510@qq.com>
2024-02-20 11:26:26 +0800

    feat: update code files

--- c++-study-2/day01/demo06/main.cpp
+++ c++-study-2/day01/demo06/main.cpp
@@ -6,6 +6,22 @@
 
 using namespace std;
 
-int main() {
+// 引用的语法：Type & 别名 = 原名
+void test01() {
+    int a = 100;
+    int &b = a;
+    b = 200;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
 
+// 引用必须初始化
+void test02() {
+
+}
+
+int main() {
+    test01();
+    test02();
+    return 0;
 }
