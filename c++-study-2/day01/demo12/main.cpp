ca8776c0a43d7fe87385d75d2b4de03acae4d804
rqh656418510 <656418510@qq.com>
2024-02-21 16:29:29 +0800

    feat: update code files

--- c++-study-2/day01/demo12/main.cpp
+++ c++-study-2/day01/demo12/main.cpp
@@ -17,26 +17,39 @@ void test01() {
 
     // 常量引用
     const int &a = 19;
-    printf("a = %d \n", a);
+
+    cout << "a = " << a << endl;
 }
 
-// 使用字面量常量对 const 引用初始化后，将生成一个只读变量，但可以使用指针的方式更改常量的值
+// 使用字面量常量对常量引用初始化后，将生成一个只读变量，但可以使用指针的方式更改常量引用的值
 void test02() {
-    // 使用字面量常量对 const 引用初始化
+    // 使用字面量常量对常量引用初始化
     const int &a = 10;
 
     // 常量引用为只读，不能直接更改值
     // a = 20;
 
-    // 但可以使用指针的方式更改常量的值
+    // 可以使用指针的方式更改常量引用的值
     int *p = (int *) &a;
     *p = 20;
     cout << "a = " << a << endl;
     cout << "*p = " << *p << endl;
 }
 
+// 使用常量引用修饰形参
+void showValue(const int &val) {
+    // val = 10;    无法直接更改形参的值，编译会不通过
+    cout << "val = " << val << endl;
+}
+
+void test03() {
+    int a = 3;
+    showValue(a);
+}
+
 int main() {
     test01();
     test02();
+    test03();
     return 0;
 }
