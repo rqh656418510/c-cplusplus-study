5a660c9b35afeb10ea26563265d303e0a88b976a
rqh656418510 <656418510@qq.com>
2024-02-21 14:59:41 +0800

    feat: update code files

--- c++-study-1/day02/demo02/main2.cpp
+++ c++-study-1/day02/demo02/main2.cpp
@@ -1,5 +1,5 @@
 /**
- * 1. C++中的引用之常引用（使用变量初始化 const 引用）
+ * 1. C++中的引用之常量引用（使用变量初始化 const 引用）
  */
 
 #include <iostream>
@@ -12,17 +12,17 @@ struct Teacher {
 };
 
 // const引用让变量(所指内存空间)拥有只读属性
-void printTe(const Name &t) {
-    // t.age = 11;   是错误写法，无法通过编译
+void printTe(const Teacher &t) {
+    // t.age = 11;  是错误写法，无法通过编译
 }
 
 // const 修饰指针和指针指向的内容，那么指针指向的内容都不能更改
-void printTe2(const Name *const pt) {
+void printTe2(const Teacher *const pt) {
     // pt->age = 11;  是错误写法，无法通过编译
 }
 
 int main() {
-    Name t1;
+    Teacher t1;
     t1.age = 33;
     printTe(t1);
     printTe2(&t1);
---
+++ c++-study-2/day01/demo12/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 1. C++ 中的常量引用
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
