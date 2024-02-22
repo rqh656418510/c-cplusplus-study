19d66111ef73c477c9bb0a3314ac6a151f9b0690
rqh656418510 <656418510@qq.com>
2024-02-22 14:26:29 +0800

    feat: update code files

--- c++-study-2/day02/demo01/main.cpp
+++ c++-study-2/day02/demo01/main.cpp
@@ -6,6 +6,45 @@
 
 using namespace std;
 
+class Circle {
+
+public:
+
+    Circle() {
+
+    }
+
+    Circle(double _radius) : radius(_radius) {
+
+    }
+
+    void setRadius(double radius) {
+        this->radius = radius;
+    }
+
+    double getRadius() {
+        return this->radius;
+    }
+
+    double getArea() {
+        this->area = 3.14 * this->radius * this->radius;
+        return this->area;
+    }
+
+private:
+
+    double radius;  // 圆形的半径
+    double area;    // 圆形的面积
+
+};
+
 int main() {
+    Circle c1(3);
+    cout << "radius = " << c1.getRadius() << " , area = " << c1.getArea() << endl;
+
+    Circle c2;
+    c2.setRadius(6);
+    cout << "radius = " << c2.getRadius() << " , area = " << c2.getArea() << endl;
+
     return 0;
 }
