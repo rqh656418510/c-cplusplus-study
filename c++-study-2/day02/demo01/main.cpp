dc29deae3044a563151a9fccf8a5994705f0e2e0
rqh656418510 <656418510@qq.com>
2024-02-22 14:02:38 +0800

    feat: update code files

---
+++ c++-study-2/day02/demo01/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 1. 作业案例-设计圆类
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ c++-study-2/day02/demo02/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 1. 作业案例-设计学生类
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
