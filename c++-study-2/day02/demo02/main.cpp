84f19b36f14ff08e7c2eee3ee91314e9b4cfff25
rqh656418510 <656418510@qq.com>
2024-02-23 13:28:27 +0800

    feat: update code files

--- c++-study-2/day02/demo02/main.cpp
+++ c++-study-2/day02/demo02/main.cpp
@@ -6,6 +6,51 @@
 
 using namespace std;
 
+class Student {
+
+public:
+    Student() {
+
+    }
+
+    Student(long _id, string _name) : id(_id), name(_name) {
+
+    }
+
+    void setId(long id) {
+        this->id = id;
+    }
+
+    void setName(string name) {
+        this->name = name;
+    }
+
+    long getId() {
+        return this->id;
+    }
+
+    string getName() {
+        return this->name;
+    }
+
+    void toString() {
+        cout << "id = " << this->getId() << ", name = " << this->getName() << endl;
+    }
+
+private:
+    long id;
+    string name;
+
+};
+
 int main() {
+    Student s1(1, "Jim");
+    s1.toString();
+
+    Student s2;
+    s2.setId(2);
+    s2.setName("Tom");
+    s2.toString();
+
     return 0;
 }
