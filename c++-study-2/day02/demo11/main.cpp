d060196bef2b4f32aec5347248eefcd88d8bdea0
rqh656418510 <656418510@qq.com>
2024-03-09 14:30:05 +0800

    feat: update code files

--- c++-study-2/day02/demo11/main.cpp
+++ c++-study-2/day02/demo11/main.cpp
@@ -6,6 +6,40 @@
 
 using namespace std;
 
+class Person {
+
+public:
+    Person() {
+        cout << "无参构造函数" << endl;
+    }
+
+    Person(string name, int age) {
+        this->name = name;
+        this->age = age;
+        cout << "有参构造函数" << endl;
+    }
+
+    Person(const Person &p) {
+        this->name = p.name;
+        this->age = p.age;
+        cout << "拷贝构造函数" << endl;
+    }
+
+private:
+    string name;
+    int age;
+
+};
+
 int main() {
+    // 自动调用无参构造函数
+    Person p1;
+
+    // 自动调用有参构造函数
+    Person p2("Jim", 23);
+
+    // 自动调用拷贝构造函数
+    Person p3 = p2;
+
     return 0;
 }
