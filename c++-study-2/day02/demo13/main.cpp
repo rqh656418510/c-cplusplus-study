fc68ad4c1be40545a7afd607649c6704345855f1
rqh656418510 <656418510@qq.com>
2024-03-10 21:59:42 +0800

    feat: update code files

--- c++-study-2/day02/demo13/main.cpp
+++ c++-study-2/day02/demo13/main.cpp
@@ -1,5 +1,9 @@
 /**
  * 构造函数的调用规则
+ *
+ * 当类中没有定义任何一个构造函数时，C++ 编译器会提供默认无参构造函数和默认拷贝构造函数
+ * 当类中定义了拷贝构造函数时，C++ 编译器不会提供默认无参构造函数
+ * 当类中定义了任意的非拷贝构造函数（即当类中定义了有参构造函数或无参构造函数），C++ 编译器不会提供默认无参构造函数
  */
 
 #include <iostream>
@@ -9,9 +13,6 @@ using namespace std;
 class Person {
 
 public:
-    Person() {
-        cout << "无参构造函数" << endl;
-    }
 
     Person(string name, int age) {
         this->name = name;
@@ -19,22 +20,16 @@ public:
         cout << "有参构造函数" << endl;
     }
 
-    Person(const Person &p) {
-        this->name = p.name;
-        this->age = p.age;
-        cout << "拷贝构造函数" << endl;
-    }
-
     ~Person() {
         cout << "析构函数" << endl;
     }
 
-    int getAge() const {
-        return this->age;
+    string getName() const {
+        return this->name;
     }
 
-    void setAge(int age) {
-        this->age = age;
+    int getAge() const {
+        return this->age;
     }
 
 private:
@@ -44,5 +39,13 @@ private:
 };
 
 int main() {
+    // 当类中定义了任意的非拷贝构造函数（即当类中定义了有参构造函数或无参构造函数），C++ 编译器不会提供默认无参构造函数
+    // Person p1;
+
+    // 当类中没有定义拷贝构造函数时，编译器默认会提供一个拷贝构造函数，用于简单地进行类成员变量的值复制
+    Person p2("Jim", 12);
+    Person p3(p2);
+    cout << "name = " << p3.getName() << ", age = " << p3.getAge() << endl;
+
     return 0;
 }
