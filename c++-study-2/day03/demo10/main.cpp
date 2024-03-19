4e7f8e7e46380e2c274505f31b6368dab7f3275a
rqh656418510 <656418510@qq.com>
2024-03-19 15:50:18 +0800

    feat: update code files

--- c++-study-2/day03/demo10/main.cpp
+++ c++-study-2/day03/demo10/main.cpp
@@ -1,36 +1,27 @@
 /**
  * explicit 关键字的作用
  *
- * <p> explicit 关键字用于声明类的有参构造函数为显式构造函数，防止其被隐式调用（即防止构造调用时进行隐式类型转换）
- * <p> 这样可以避免一些意外的类型转换和编译器自动调用构造函数的情况
+ * <p> 在 C++ 中，`explicit` 关键字用于声明类的单参数构造函数为显式构造函数，防止其被隐式调用。
+ * <p> 简而言之，`explicit` 关键字可以显式地禁止类构造函数的隐式转换，这样可以避免一些意外的类型转换和编译器自动调用构造函数的情况。
  */
 
 #include <iostream>
 
 using namespace std;
 
-class MyString {
+class MyClass {
 
 public:
-    MyString(char *str) {
-        m_str = str;
-    }
+    explicit MyClass(int x) : num(x) {
 
-    explicit MyString(int size) {
-        m_size = size;
     }
 
 private:
-    char *m_str;
-    int m_size;
+    int num;
+
 };
 
 int main() {
-    MyString str("abc");
-    MyString str2 = "abc";
-
-    MyString str3(10);
-    // MyString str4 = 10;     // 默认情况下，这里会发生隐式类型转换，使用 explicit 关键字修饰函数后，可以禁止隐式类型转换
-
-    return 0;
+    // MyClass obj = 42;   // 错误：不允许隐式转换
+    MyClass obj2(42);   // 正确：显式转换
 }
