adcb4c9720e167a2f090bbcdb7eb35e18cb27eb0
rqh656418510 <656418510@qq.com>
2024-03-26 00:26:30 +0800

    feat: update code files

--- c++-study-2/day04/demo01/main.cpp
+++ c++-study-2/day04/demo01/main.cpp
@@ -6,6 +6,47 @@
 
 using namespace std;
 
+class Counter {
+
+public:
+
+    Counter() {
+
+    }
+
+    Counter(const Counter &p) {
+        this->number = p.number;
+    }
+
+    ~Counter() {
+
+    }
+
+    void setNumber(const int num) {
+        this->number = num;
+    }
+
+    void showNumber() const {
+        cout << "number = " << this->number << endl;
+    }
+
+private:
+    static int number;
+
+};
+
+// 定义静态成员变量，这里不是简单的变量赋值，更重要的是告诉C++编译器，给静态成员变量分配内存
+int Counter::number = 0;
+
+int a;
+
 int main() {
+    Counter c1;
+    c1.setNumber(10);
+    c1.showNumber();
+
+    Counter c2;
+    c2.showNumber();
+
     return 0;
 }
