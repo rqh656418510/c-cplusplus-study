840f2a3b241cf090c9adac4b9cf16a18bd192cc9
rqh656418510 <656418510@qq.com>
2024-03-31 18:24:50 +0800

    feat: update code files

--- c++-study-2/day04/demo04/main.cpp
+++ c++-study-2/day04/demo04/main.cpp
@@ -3,9 +3,60 @@
  */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class Printer {
+
+private:
+
+    // 私有的默认构造函数
+    Printer() {
+
+    }
+
+    // 私有的拷贝造函数
+    Printer(const Printer &p) {
+
+    }
+
+public:
+    void printText(string str) {
+        cout << str << endl;
+        times++;
+    }
+
+    long getTimes() {
+        return times;
+    }
+
+public:
+    static Printer *getInstance() {
+        return singleton;
+    }
+
+private:
+    // 打印次数
+    long times = 0;
+
+    // 声明静态变量（单例）
+    static Printer *singleton;
+};
+
+// 初始化静态变量（单例），分配内存空间
+Printer *Printer::singleton = new Printer();
+
 int main() {
+    Printer *printer1 = Printer::getInstance();
+    Printer *printer2 = Printer::getInstance();
+    // 判断内存地址是否相同
+    cout << (printer1 == printer2 ? "true" : "false") << endl;
+
+    printer1->printText("C++ Guilde");
+    printer1->printText("Think in Java");
+    long times = printer1->getTimes();
+    cout << "times = " << times << endl;
+
     return 0;
 }
