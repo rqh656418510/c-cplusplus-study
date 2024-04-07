ce863f03d6be64168d1a02164137968e800fd6a2
rqh656418510 <656418510@qq.com>
2024-04-07 23:29:35 +0800

    feat: update code files

--- c++-study-2/day05/demo01/main.cpp
+++ c++-study-2/day05/demo01/main.cpp
@@ -8,21 +8,27 @@ using namespace std;
 
 void test01() {
     MyArray *myArray1 = new MyArray(20);
-    MyArray myArray2 = *myArray1;
+
+    // 第一种写法，会自动调用拷贝构造函数
+    // MyArray myArray2 = *myArray1;
+
+    // 第二种写法，会自动调用拷贝构造函数
+    MyArray *myArray2 = new MyArray(*myArray1);
 
     // 尾插数据
     for (int i = 0; i < 10; i++) {
-        myArray2.push_back(i);
+        myArray2->push_back(i);
     }
 
     // 设置数据
-    myArray2.set_data(3, 42);
+    myArray2->set_data(3, 42);
 
     // 获取数据
     for (int i = 0; i < 10; i++) {
-        cout << myArray2.get_data(i) << endl;
+        cout << myArray2->get_data(i) << endl;
     }
 
+    delete myArray2;
     delete myArray1;
 }
 
