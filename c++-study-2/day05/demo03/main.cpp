884029585938e3c8d47d13a24eaf7fa49b2ff085
rqh656418510 <656418510@qq.com>
2024-04-10 00:24:22 +0800

    feat: update code files

--- c++-study-2/day05/demo03/main.cpp
+++ c++-study-2/day05/demo03/main.cpp
@@ -6,6 +6,36 @@
 
 using namespace std;
 
-int main() {
+class Person {
 
+public:
+    Person() {
+        this->m_A = 0;
+        this->m_B = 0;
+    }
+
+    Person(int a, int b) {
+        this->m_A = a;
+        this->m_B = b;
+    }
+
+    // 声明友元函数
+    friend ostream &operator<<(ostream &out, Person &p);
+
+private:
+    int m_A;
+    int m_B;
+
+};
+
+// 使用友元函数重载左移运算符
+ostream &operator<<(ostream &out, Person &p) {
+    out << "a = " << p.m_A << ", b = " << p.m_B << endl;
+    return out;
+}
+
+int main() {
+    Person p(1, 2);
+    // 重载左移运算符
+    cout << p << endl;
 }
