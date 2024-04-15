81783c8dac17818f11c63bba58425acb65c5fbfb
rqh656418510 <656418510@qq.com>
2024-04-15 22:47:30 +0800

    feat: update code files

--- c++-study-2/day05/demo08/main.cpp
+++ c++-study-2/day05/demo08/main.cpp
@@ -5,6 +5,42 @@
 
 using namespace std;
 
+class Person {
+
+public:
+    Person(string name, int age) : m_name(name), m_Age(age) {
+
+    }
+
+    bool operator==(Person p) {
+        return this->m_name == p.m_name && this->m_Age == p.m_Age;
+    }
+
+    bool operator>(Person p) {
+        return this->m_Age > p.m_Age;
+    }
+
+    bool operator<(Person p) {
+        return this->m_Age < p.m_Age;
+    }
+
+private :
+    string m_name;
+    int m_Age;
+
+};
+
+void test01() {
+    Person p1("Tom", 18);
+    Person p2("Peter", 20);
+    Person p3("Peter", 20);
+
+    cout << (p1 < p2) << endl;
+    cout << (p1 > p2) << endl;
+    cout << (p2 == p3) << endl;
+}
+
 int main() {
+    test01();
     return 0;
 }
---
+++ c++-study-2/day05/demo09/main.cpp
@@ -0,0 +1,45 @@
+/**
+ * 函数运算符重载
+ */
+#include <iostream>
+
+using namespace std;
+
+class MyPrint {
+
+public:
+    void operator()(const string &content) {
+        cout << content << endl;
+    }
+
+};
+
+void test01() {
+    MyPrint myPrint;
+    myPrint("Hello World");
+}
+
+class MyAdd {
+
+public:
+    int operator()(const int a, const int b) {
+        return a + b;
+    }
+
+};
+
+void test02() {
+    MyAdd add;
+
+    int sum = add(1, 2);
+    cout << sum << endl;
+
+    // 匿名对象
+    cout << MyAdd()(3, 4) << endl;
+}
+
+int main() {
+    test01();
+    test02();
+    return 0;
+}
