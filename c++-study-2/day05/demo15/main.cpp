e52cc02e263159c9c267debb685f7875ce1307d9
rqh656418510 <656418510@qq.com>
2024-04-22 22:24:14 +0800

    feat: update code files

--- c++-study-2/day05/demo15/main.cpp
+++ c++-study-2/day05/demo15/main.cpp
@@ -6,6 +6,55 @@
 
 using namespace std;
 
+class Base {
+
+public:
+    Base() {
+        this->m_A = 100;
+    }
+
+    void print() {
+        cout << m_A << endl;
+    }
+
+public:
+    int m_A;
+
+};
+
+class Son : public Base {
+
+public:
+    Son() {
+        this->m_A = 200;
+    }
+
+    void print() {
+        cout << m_A << endl;
+    }
+
+public:
+    int m_A;
+
+};
+
+void test01() {
+    Son son;
+    cout << son.m_A << endl;
+    // 继承中同名变量的处理，访问父类的成员属性
+    cout << son.Base::m_A << endl;
+}
+
+void test02() {
+    Son son;
+    son.print();
+    // 继承中同名函数的处理，访问父类的成员函数
+    son.Base::print();
+}
+
 int main() {
+    test01();
+    cout << "----------" << endl;
+    test02();
     return 0;
 }
