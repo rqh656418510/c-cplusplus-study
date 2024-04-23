a5f6df73efd1dd9ce1953313cb5fb25b4f3b3278
rqh656418510 <656418510@qq.com>
2024-04-23 21:42:00 +0800

    feat: update code files

--- c++-study-2/day05/demo16/main.cpp
+++ c++-study-2/day05/demo16/main.cpp
@@ -6,6 +6,53 @@
 
 using namespace std;
 
+class Base {
+
+public:
+    static void func() {
+        cout << "Base.func()" << endl;
+    }
+
+public:
+    static int m_A;
+
+};
+
+int Base::m_A = 10;
+
+class Son : public Base {
+
+public:
+    static void func() {
+        cout << "Son.func()" << endl;
+    }
+
+public:
+    static int m_A;
+
+};
+
+int Son::m_A = 20;
+
+void test01() {
+    // 访问父类的静态成员属性
+    cout << Base::m_A << endl;
+
+    // 访问子类的静态成员属性
+    cout << Son::m_A << endl;
+}
+
+void test02() {
+    // 访问父类的静态成员函数
+    Base::func();
+    Son::Base::func();
+
+    // 访问子类的静态成员函数
+    Son::func();
+}
+
 int main() {
+    test01();
+    test02();
     return 0;
 }
---
+++ c++-study-2/day05/demo17/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 多继承的概念以及问题
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
