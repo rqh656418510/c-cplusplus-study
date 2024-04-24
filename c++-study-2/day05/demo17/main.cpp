69121ac8c69eebeee0834df73349b5302816d92f
rqh656418510 <656418510@qq.com>
2024-04-24 21:46:44 +0800

    feat: update code files

--- c++-study-2/day05/demo17/main.cpp
+++ c++-study-2/day05/demo17/main.cpp
@@ -6,6 +6,58 @@
 
 using namespace std;
 
+class Base1 {
+
+public:
+    Base1(int a) {
+        this->m_A = a;
+    }
+
+public:
+    int m_A;
+
+};
+
+class Base2 {
+
+public:
+    Base2(int a) {
+        this->m_A = a;
+    }
+
+public:
+    int m_A;
+
+};
+
+// 多继承
+class Son : public Base1, public Base2 {
+
+public:
+    Son(int a, int b) : Base1(a), Base2(a) {
+        this->m_B = b;
+    }
+
+public :
+    int m_B;
+
+};
+
+void test01() {
+    cout << sizeof(Son) << endl;
+}
+
+void test02() {
+    Son son(1, 2);
+    cout << son.m_B << endl;
+
+    // 解决多继承中的二义性问题
+    cout << son.Base1::m_A << endl;
+    cout << son.Base2::m_A << endl;
+}
+
 int main() {
+    test01();
+    test02();
     return 0;
 }
