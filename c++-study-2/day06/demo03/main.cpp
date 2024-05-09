1e1777d4cd215267f401a7d86e3bd9a78fe8271d
rqh656418510 <656418510@qq.com>
2024-05-09 21:54:07 +0800

    feat: update code files

--- c++-study-2/day06/demo03/main.cpp
+++ c++-study-2/day06/demo03/main.cpp
@@ -1,11 +1,76 @@
 /**
- * 多态深入剖析锻炼
+ * 多态案例-计算器案例
  */
 
 #include <iostream>
 
 using namespace std;
 
+// 抽象计算器
+class AbstractCalculator {
+
+public:
+    // 定义虚函数
+    virtual int getResult() {
+        return 0;
+    }
+
+    int getValue1() const {
+        return this->value1;
+    }
+
+    void setValue1(int value1) {
+        this->value1 = value1;
+    }
+
+    int getValue2() const {
+        this->value2;
+    }
+
+    void setValue2(int value2) {
+        this->value2 = value2;
+    }
+
+public:
+    int value1;
+    int value2;
+
+};
+
+// 加法计算器
+class AdditionCalculator : public AbstractCalculator {
+
+public:
+    // 虚函数重写
+    virtual int getResult() override {
+        return this->value1 + this->value2;
+    }
+
+};
+
+// 减法计算器
+class SubtractionCalculator : public AbstractCalculator {
+
+public:
+    // 虚函数重写
+    virtual int getResult() override {
+        return this->value1 - this->value2;
+    }
+
+};
+
 int main() {
+    AbstractCalculator *calculator;
+
+    calculator = new AdditionCalculator();
+    calculator->setValue1(3);
+    calculator->setValue2(5);
+    cout << calculator->getResult() << endl;
+
+    calculator = new SubtractionCalculator();
+    calculator->setValue1(3);
+    calculator->setValue2(5);
+    cout << calculator->getResult() << endl;
+
     return 0;
 }
