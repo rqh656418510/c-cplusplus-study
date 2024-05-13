1f0cfcebc372f1d51c0304c2fe5ce3ab08203276
rqh656418510 <656418510@qq.com>
2024-05-13 21:46:57 +0800

    feat: update code files

---
+++ c++-study-2/day06/demo05/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 虚析构和纯虚析构函数
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ c++-study-2/day06/demo06/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 向上类型转换和向下类型转换
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
