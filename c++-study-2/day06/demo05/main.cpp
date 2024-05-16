6580c8fad0fd4cd4b5ce82fa5b49bd051e06a3d5
rqh656418510 <656418510@qq.com>
2024-05-17 00:58:04 +0800

    feat: update code files

--- c++-study-2/day06/demo05/main.cpp
+++ c++-study-2/day06/demo05/main.cpp
@@ -1,11 +1,62 @@
 /**
- * 虚析构和纯虚析构函数
+ * 虚析构函数
+ *
+ * <p> 虚析构函数使得在删除指向子类对象的父类指针时，可以调用子类的析构函数来实现释放子类中堆内存的目的，从而防止内存泄漏
  */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+class Animal {
+
+public :
+    virtual void speak() {
+        cout << "Animal speaking ..." << endl;
+    }
+
+    // 定义虚析构函数
+    virtual ~Animal() {
+        cout << "调用 Animal 的析构函数" << endl;
+    }
+
+};
+
+class Cat : public Animal {
+
+public:
+    Cat(char *name) {
+        // 分配内存空间
+        this->m_Name = new char[strlen(name) + 1];
+        strcpy(this->m_Name, name);
+    }
+
+    virtual void speak() {
+        cout << "Cat speaking ..." << endl;
+    }
+
+    ~Cat() {
+        cout << "调用 Cat 的析构函数" << endl;
+        // 释放内存空间
+        if (this->m_Name != nullptr) {
+            delete[] this->m_Name;
+            this->m_Name = nullptr;
+        }
+    }
+
+public:
+    char *m_Name;
+
+};
+
+void test01() {
+    Animal *animal = new Cat("Tom");
+    animal->speak();
+    delete animal;
+}
+
 int main() {
+    test01();
     return 0;
 }
--- c++-study-2/day06/demo06/main.cpp
+++ c++-study-2/day06/demo06/main.cpp
@@ -1,11 +1,60 @@
 /**
- * 向上类型转换和向下类型转换
+ * 纯虚析构函数
  */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+class Animal {
+
+public :
+    virtual void speak() {
+        cout << "Animal speaking ..." << endl;
+    }
+
+    // 定义虚析构函数
+    virtual ~Animal() {
+        cout << "调用 Animal 的析构函数" << endl;
+    }
+
+};
+
+class Cat : public Animal {
+
+public:
+    Cat(char *name) {
+        // 分配内存空间
+        this->m_Name = new char[strlen(name) + 1];
+        strcpy(this->m_Name, name);
+    }
+
+    virtual void speak() {
+        cout << "Cat speaking ..." << endl;
+    }
+
+    ~Cat() {
+        cout << "调用 Cat 的析构函数" << endl;
+        // 释放内存空间
+        if (this->m_Name != nullptr) {
+            delete[] this->m_Name;
+            this->m_Name = nullptr;
+        }
+    }
+
+public:
+    char *m_Name;
+
+};
+
+void test01() {
+    Animal *animal = new Cat("Tom");
+    animal->speak();
+    delete animal;
+}
+
 int main() {
+    test01();
     return 0;
 }
