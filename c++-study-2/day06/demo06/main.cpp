ed977c0def4edd52dcf3dca990541dd040273a4c
rqh656418510 <656418510@qq.com>
2024-05-17 01:07:01 +0800

    feat: update code files

--- c++-study-2/day06/demo06/main.cpp
+++ c++-study-2/day06/demo06/main.cpp
@@ -1,5 +1,8 @@
 /**
  * 纯虚析构函数
+ *
+ * <p> 纯虚析构函数必须在类内声明，类外实现
+ * <p> 如果类拥有纯析构函数，那么这个类也算是抽象类，即该类不能实例化对象
  */
 
 #include <iostream>
@@ -14,18 +17,21 @@ public :
         cout << "Animal speaking ..." << endl;
     }
 
-    // 定义虚析构函数
-    virtual ~Animal() {
-        cout << "调用 Animal 的析构函数" << endl;
-    }
+    // 声明纯虚析构函数
+    virtual ~Animal() = 0;
 
 };
+
+// 定义纯虚析构函数
+Animal::~Animal() {
+    cout << "调用 Animal 的析构函数" << endl;
+}
 
 class Cat : public Animal {
 
 public:
     Cat(char *name) {
-        // 分配内存空间
+        // 分配内存空间（必须在类内声明，类外实现）
         this->m_Name = new char[strlen(name) + 1];
         strcpy(this->m_Name, name);
     }
@@ -49,6 +55,10 @@ public:
 };
 
 void test01() {
+    // 错误写法，不能实例化对象
+    // 如果类拥有纯析构函数，那么这个类也算是抽象类，即该类不能实例化对象
+    // Animal *animal = new Animal();
+
     Animal *animal = new Cat("Tom");
     animal->speak();
     delete animal;
