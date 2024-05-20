e0d58cd3534caf6af7d5f17860dec65b325f95d7
rqh656418510 <656418510@qq.com>
2024-05-20 22:44:31 +0800

    feat: update code files

--- c++-study-2/day06/demo07/main.cpp
+++ c++-study-2/day06/demo07/main.cpp
@@ -6,6 +6,38 @@
 
 using namespace std;
 
+class Base {
+
+public:
+    virtual void foo() {
+
+    }
+
+};
+
+class Derived : public Base {
+
+};
+
+void test01() {
+    Derived derived;
+    Base *basePtr = &derived;   // 向上类型转换，派生类转换为基类（安全）
+}
+
+void test02() {
+    Base baseObj;
+    Base *basePtr = &baseObj;
+
+    // 向下类型转换，基类转换为派生类（不安全）
+    Derived *derivedPtr = dynamic_cast<Derived *>(basePtr);
+    if (derivedPtr) {
+        // 转换成功
+    } else {
+        // 转换失败
+    }
+}
+
 int main() {
-    return 0;
+    test01();
+    test02();
 }
