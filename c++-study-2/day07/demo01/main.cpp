9c46a737e4c497cb56c7944111f37939db8874a3
rqh656418510 <656418510@qq.com>
2024-06-08 23:16:40 +0800

    feat: update code files

--- c++-study-2/day07/demo01/main.cpp
+++ c++-study-2/day07/demo01/main.cpp
@@ -1,11 +1,36 @@
 /**
- * 函数模板基本使用
+ * 函数模板的基础使用
  */
 
 #include <iostream>
 
 using namespace std;
 
+// 模板声明
+template<typename T>
+
+// 函数定义
+void mySwap(T &t1, T &t2) {
+    T tmp = t1;
+    t1 = t2;
+    t2 = tmp;
+}
+
 int main() {
+    // 自动数据类型推导
+    int x = 3, y = 5;
+    mySwap(x, y);
+    cout << "x = " << x << ", y = " << y << endl;
+
+    // 自动数据类型推导
+    double m = 1.2, n = 5.6;
+    mySwap(m, n);
+    cout << "m = " << m << ", n = " << n << endl;
+
+    // 显示指定类型调用（推荐）
+    char c1 = 'A', c2 = 'B';
+    mySwap<char>(c1, c2);
+    cout << "c1 = " << c1 << ", c2 = " << c2 << endl;
+
     return 0;
 }
