2e28ee89f3c8b9f9748658406946929398849948
rqh656418510 <656418510@qq.com>
2024-06-25 23:10:48 +0800

    feat: update code files

--- c++-study-2/day07/demo05/main.cpp
+++ c++-study-2/day07/demo05/main.cpp
@@ -6,6 +6,48 @@
 
 using namespace std;
 
+class Person {
+
+public:
+
+    Person(string name, int age) {
+        this->m_name = name;
+        this->m_age = age;
+    }
+
+    string m_name;
+    int m_age;
+
+};
+
+template<class T>
+bool myCompare(T &a, T &b) {
+    return a == b;
+}
+
+void test01() {
+    int a = 10;
+    int b = 20;
+    bool resutlt = myCompare(a, b); // 传入基础数据类型
+    cout << resutlt << endl;
+}
+
+// 通过第三代具体化自定义数据类型，解决函数模板的局限性问题
+// 如果具体化能够优先匹配，那么就选择具体化
+template<> bool myCompare<Person>(Person &a, Person &b) {
+    return a.m_age == b.m_age;
+}
+
+void test02() {
+    Person p1("Tom", 23);
+    Person p2("Jim", 23);
+
+    bool resutlt = myCompare(p1, p2);  // 传入自定义数据类型
+    cout << resutlt << endl;
+}
+
 int main() {
+    test01();
+    test02();
     return 0;
 }
