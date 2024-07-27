a6fb5f6274c0633078909b2771774b0213a8d0b4
rqh656418510 <656418510@qq.com>
2024-07-28 01:05:53 +0800

    Update files

---
+++ c++-study-2/day07/demo12/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 友元碰到类模板-友元函数类内实现
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ c++-study-2/day07/demo13/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 类模板的应用-数组类的封装
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
