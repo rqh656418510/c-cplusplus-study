c118c1747e77c3ed195f530e818c84c171a2af40
rqh656418510 <656418510@qq.com>
2024-08-03 20:50:18 +0800

    Update files

---
+++ c++-study-2/day08/demo02/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 类型转换-动态类型
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ c++-study-2/day08/demo03/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 类型转换-静态和重新解释转换
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
