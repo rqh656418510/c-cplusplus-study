e4b683da8e21a4d03dde55289086540c86af8664
rqh656418510 <656418510@qq.com>
2024-08-18 17:22:10 +0800

    Update files

---
+++ c++-study-2/day08/demo09/main2.cpp
@@ -0,0 +1,51 @@
+/**
+ * 异常变量的生命周期
+ */
+#include <iostream>
+
+using namespace std;
+
+class MyException {
+
+public:
+
+    MyException() {
+        cout << "调用构造函数" << endl;
+    }
+
+    MyException(const MyException &e) {
+        cout << "调用拷贝构造函数" << endl;
+    }
+
+    ~MyException() {
+        cout << "调用析构函数" << endl;
+    }
+
+    void printError() {
+        cout << "Exception Message: null" << endl;
+    }
+
+};
+
+void doWork() {
+    MyException exception;
+    throw &exception;   // 相当于 throw &(MyException());
+}
+
+int main() {
+    try {
+        doWork();
+    }
+    catch (MyException *e) {
+
+        // 执行到这里的时候，自定义异常类的析构函数已经被调用，即指针 e 已经被释放了，不能再继续使用
+        // e->printError();
+
+        cout << "捕获到自定义异常" << endl;
+    }
+    catch (...) {
+        cout << "捕获到未知异常" << endl;
+    }
+
+    return 0;
+}
---
+++ c++-study-2/day08/demo09/main3.cpp
@@ -0,0 +1,52 @@
+/**
+ * 异常变量的生命周期
+ */
+#include <iostream>
+
+using namespace std;
+
+class MyException {
+
+public:
+
+    MyException() {
+        cout << "调用构造函数" << endl;
+    }
+
+    MyException(const MyException &e) {
+        cout << "调用拷贝构造函数" << endl;
+    }
+
+    ~MyException() {
+        cout << "调用析构函数" << endl;
+    }
+
+    void printError() {
+        cout << "Exception Message: null" << endl;
+    }
+
+};
+
+void doWork() {
+    throw new MyException();
+}
+
+int main() {
+    try {
+        doWork();
+    }
+    catch (MyException *e) {
+        // 自定义异常类的指针 e 可以继续使用，因为自定义异常类的析构函数还没有被调用
+        e->printError();
+
+        // 需要手动调用自定义异常类的析构函数来释放内存
+        delete e;
+
+        cout << "捕获到自定义异常" << endl;
+    }
+    catch (...) {
+        cout << "捕获到未知异常" << endl;
+    }
+
+    return 0;
+}
