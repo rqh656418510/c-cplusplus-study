15ed64ae0d6a9f5275d9cc913aecaf1ad5a611e5
rqh656418510 <656418510@qq.com>
2024-08-18 15:10:10 +0800

    Update files

--- c++-study-2/day08/demo06/main.cpp
+++ c++-study-2/day08/demo06/main.cpp
@@ -5,6 +5,40 @@
 
 using namespace std;
 
+// 自定义异常类
+class MyException {
+
+public:
+    MyException(string message) {
+        this->message = message;
+    }
+
+    void printError() {
+        cout << "Exception: " << this->message << endl;
+    }
+
+private:
+    string message;
+
+};
+
+int devide(int a, int b) {
+    if (b == 0) {
+        // 抛出自定义异常
+        throw MyException("被除数不能为零");   // 匿名对象的写法
+    }
+    return a / b;
+}
+
+
 int main() {
+    try {
+        int result = devide(9, 0);
+        cout << "result = " << result << endl;
+    } catch (MyException e) {
+        e.printError();
+    }
+
+    cout << "程序正常结束运行" << endl;
     return 0;
 }
