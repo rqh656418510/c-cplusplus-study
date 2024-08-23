ca7dad3733575172f35e09ccb38f3e95e1b44372
rqh656418510 <656418510@qq.com>
2024-08-24 01:22:10 +0800

    Update files

--- c++-study-2/day08/demo12/main.cpp
+++ c++-study-2/day08/demo12/main.cpp
@@ -2,9 +2,60 @@
  * 编写自己的异常类
  */
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
+// 继承自标准异常库的异常类
+class MyOutOfRangeException : public exception {
+
+public:
+    explicit MyOutOfRangeException(const string &str) {
+        this->m_Error = str.c_str();
+    }
+
+    virtual ~MyOutOfRangeException() {
+
+    }
+
+    virtual const char *what() {
+        return this->m_Error;
+    }
+
+private:
+    const char *m_Error;
+
+};
+
+class Person {
+
+public:
+    Person(string name, int age) {
+        if (age < 0 || age > 150) {
+            // 抛出自定义异常类
+            throw MyOutOfRangeException("年龄超出范围");
+        }
+
+        this->m_Name = name;
+        this->m_Age = age;
+    }
+
+private:
+    string m_Name;
+    int m_Age;
+
+};
+
 int main() {
+    try {
+        Person person("Tom", 200);
+    }
+    catch (MyOutOfRangeException &e) {
+        cout << e.what() << endl;
+    }
+    catch (...) {
+        cout << "发生未知类型的异常!" << endl;
+    }
+
     return 0;
 }
