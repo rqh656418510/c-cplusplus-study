3b0c430a6711980e5ebc970f83f5926b8ccc2236
rqh656418510 <656418510@qq.com>
2024-08-23 22:52:47 +0800

    Update files

--- c++-study-2/day08/demo10/main.cpp
+++ c++-study-2/day08/demo10/main.cpp
@@ -5,6 +5,34 @@
 
 using namespace std;
 
+class BaseException {
+
+public:
+    virtual void printError() {
+        cout << "Base Exception" << endl;
+    }
+
+};
+
+class NullPointerException : public BaseException {
+
+    void printError() override {
+        cout << "Null Pointer Exception" << endl;
+    }
+
+};
+
+void doWrok() {
+    throw NullPointerException();
+}
+
 int main() {
+    try {
+        doWrok();
+    }
+    catch (BaseException &e) {
+        // 这里会发生多态，即父类的引用指向子类的对象
+        e.printError();
+    }
     return 0;
 }
