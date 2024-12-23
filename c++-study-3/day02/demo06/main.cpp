efd96a33791e9742aaa7441e3192bfb2c79cdc14
rqh656418510 <656418510@qq.com>
2024-12-23 15:22:11 +0800

    Update files

---
+++ c++-study-3/day02/demo06/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * 类和对象代码应用实践
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+}
---
+++ c++-study-3/day02/demo07/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * 掌握类的各种成员方法以及区别
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+}
