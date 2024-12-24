6ff0a1a9132bdfe7909386c177a0e423ac39a536
rqh656418510 <656418510@qq.com>
2024-12-24 18:54:59 +0800

    Update files

--- c++-study-3/day03/demo04/main.cpp
+++ c++-study-3/day03/demo04/main.cpp
@@ -6,6 +6,147 @@
 
 using namespace std;
 
+// 类模板
+template<typename T>
+// 向量容器
+class MyVectorr {
+
+public:
+    // 构造函数
+    MyVectorr(int size = 10) {
+        _first = new T[size];
+        _last = _first;
+        _end = _first + size;
+    }
+
+    // 析构函数
+    ~MyVectorr() {
+        if (_first != nullptr) {
+            delete[] _first;
+            _first = _last = _end = nullptr;
+        }
+    }
+
+    // 拷贝构造函数
+    MyVectorr(const MyVectorr<T> &v) {
+        // 容器的总大小
+        int size = v._end - v._first;
+        // 有效元素的个数
+        int length = v._last - v._first;
+        // 深拷贝
+        _first = new T[size];
+        for (int i = 0; i < length; i++) {
+            _first[i] = v._first[i];
+        }
+        _last = _first + length;
+        _end = _first + size;
+    }
+
+    // 赋值运算符重载
+    MyVectorr<T> &operator=(const MyVectorr<T> &v) {
+        if (this == v) {
+            return *this;
+        }
+
+        // 释放原来的内存空间
+        if (_first != nullptr) {
+            delete[] _first;
+        }
+
+        // 容器的总大小
+        int size = v._end - v._first;
+        // 有效元素的个数
+        int length = v._last - v._first;
+        // 深拷贝
+        _first = new T[size];
+        for (int i = 0; i < length; i++) {
+            _first[i] = v._first[i];
+        }
+        _last = _first + length;
+        _end = _first + size;
+    }
+
+    // 往容器尾部添加元素
+    void push(const T &val) {
+        if (full()) {
+            resize();
+        }
+        *_last++ = val;
+    }
+
+    // 从容器尾部删除元素
+    void pop_back() {
+        if (!empty()) {
+            --_last;
+        }
+    }
+
+    // 返回容器尾部的元素
+    T back() const {
+        if (empty()) {
+            throw "MyVectorr is empty!";
+        }
+        return *(_last - 1);
+    }
+
+    // 容器是否满了
+    bool full() const {
+        return _last == _end;
+    }
+
+    // 容器是否为空
+    bool empty() const {
+        return _first == _last;
+    }
+
+    // 返回有效元素的个数
+    int size() {
+        return _last - _first;
+    }
+
+private:
+    T *_first;  // 指向数组起始的位置
+    T *_last;   // 指向数组中有效元素的后继位置
+    T *_end;    // 指向数组空间的后继位置
+
+    // 扩容操作
+    void resize() {
+        int size = _end - _first;
+        T *_ptemp = new T[size * 2];
+
+        for (int i = 0; i < size; i++) {
+            _ptemp[i] = _first[i];
+        }
+
+        // 释放原来的内存空间
+        delete[] _first;
+
+        _first = _ptemp;
+        _last = _first + size;
+        _end = _first + size * 2;
+    }
+};
+
 int main() {
+    // 设置随机数种子
+    srand(time(nullptr));
+
+    MyVectorr<int> v;
+    for (int i = 0; i < 20; i++) {
+        int val = random() % 100;
+        v.push(val);
+        cout << val << " ";
+    }
+    cout << endl;
+
+    cout << "size: " << v.size() << endl;
+    cout << "full: " << (v.full() ? "true" : " false") << endl;
+    cout << "empty: " << (v.empty() ? "true" : " false") << endl;
+
+    while (!v.empty()) {
+        cout << v.back() << " ";
+        v.pop_back();
+    }
+
     return 0;
 }
