c71e960c2f10691a4111d08e623b7dc7154fe002
rqh656418510 <656418510@qq.com>
2025-01-14 18:38:29 +0800

    Update files

--- c++-study-3/day04/demo06/main.cpp
+++ c++-study-3/day04/demo06/main.cpp
@@ -1,10 +1,71 @@
 /**
  * 深入理解new的delete的原理
+ *
+ * <p> 生成汇编代码：g++ -S -o main.s main.cpp
  */
+
 #include <iostream>
 
 using namespace std;
 
+// 重写 new 运算符，先调用 operator new 开发内存空间，然后再调用对象的构造函数（初始化）
+void *operator new(size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == nullptr) {
+        throw bad_alloc();
+    }
+    cout << "operator new address: " << ptr << endl;
+    return ptr;
+}
+
+// 重写 delete 运算符，先调用 ptr 指向对象的析构函数，然后再调用 operator delete 释放内存空间
+void operator delete(void *ptr) {
+    if (ptr != nullptr) {
+        free(ptr);
+        cout << "operator delete address: " << ptr << endl;
+    }
+}
+
+// 重写 new[] 运算符，先调用 operator new 开发内存空间，然后再调用对象的构造函数（初始化）
+void *operator new[](size_t size) {
+    void *ptr = malloc(size);
+    if (ptr == nullptr) {
+        throw bad_alloc();
+    }
+    cout << "operator new[] address: " << ptr << endl;
+    return ptr;
+}
+
+// 重写 delete[] 运算符，先调用 ptr 指向对象的析构函数，然后再调用 operator delete 释放内存空间
+void operator delete[](void *ptr) {
+    if (ptr != nullptr) {
+        free(ptr);
+        cout << "operator delete[] address: " << ptr << endl;
+    }
+}
+
+void test01() {
+    try {
+        // 调用重载后的 new 和 delete 运算符
+        int *p = new int;
+        delete p;
+    } catch (const bad_alloc &exception) {
+        cerr << exception.what() << endl;
+    }
+}
+
+void test02() {
+    try {
+        // 调用重载后的 new[] 和 delete[] 运算符
+        int *p = new int[10];
+        delete[] p;
+    } catch (const bad_alloc &exception) {
+        cerr << exception.what() << endl;
+    }
+}
+
 int main() {
+    test01();
+    test02();
     return 0;
 }
