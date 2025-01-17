2eca06e6044422cb433de2aed396b4a06783b8ba
rqh656418510 <656418510@qq.com>
2025-01-17 13:57:11 +0800

    Update files

--- c++-study-3/day05/demo02/main.cpp
+++ c++-study-3/day05/demo02/main.cpp
@@ -6,6 +6,40 @@
 
 using namespace std;
 
+class Base {
+
+public:
+    Base(int data) : ma(data) {
+        cout << "Base()" << endl;
+    }
+
+    ~Base() {
+        cout << "~Base()" << endl;
+    }
+
+protected:
+    int ma;
+
+};
+
+class Device : public Base {
+
+public:
+    // 显式调用 Base 的构造函数初始化 ma
+    Device(int data) : Base(data), mb(data) {
+        cout << "Device()" << endl;
+    }
+
+    ~Device() {
+        cout << "~Device()" << endl;
+    }
+
+private:
+    int mb;
+
+};
+
 int main() {
+    Device d(2);    // 先构造的后析构，即后构造的先析构
     return 0;
 }
