91ac97fe74fa061df088a417586c6f119a97efd3
rqh656418510 <656418510@qq.com>
2025-01-18 22:32:35 +0800

    Update files

---
+++ c++-study-3/day05/demo06/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 理解多态到底是什么
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ c++-study-3/day05/demo07/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 理解抽象类
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
