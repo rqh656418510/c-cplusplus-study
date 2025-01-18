e62b27e837c00eab82bd933355e711385cd9dc9d
rqh656418510 <656418510@qq.com>
2025-01-18 22:25:26 +0800

    Update files

--- c++-study-3/day05/demo05/main.cpp
+++ c++-study-3/day05/demo05/main.cpp
@@ -6,6 +6,49 @@
 
 using namespace std;
 
+class Base {
+
+public:
+    Base(int data) : ma(data) {
+        cout << "Base()" << endl;
+    }
+
+    // 虚析构函数
+    virtual ~Base() {
+        cout << "~Base()" << endl;
+    }
+
+    // 虚函数
+    virtual void show() {
+        cout << "Base::show()" << endl;
+    }
+
+private :
+    int ma;
+
+};
+
+class Device : public Base {
+
+public:
+    Device(int data) : Base(data), mb(data) {
+        cout << "Device()" << endl;
+    }
+
+    // 当基类的析构函数是 virtual 虚函数，那么派生类的析构函数会自动成为虚函数
+    ~Device() {
+        cout << "~Device()" << endl;
+    }
+
+private:
+    int mb;
+
+};
+
 int main() {
+    Base *pb = new Device(10);
+    pb->show();
+    delete pb;
+
     return 0;
 }
