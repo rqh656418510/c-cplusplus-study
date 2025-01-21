67d29d44eaaa7c74dac7dac72441b812503416fc
rqh656418510 <656418510@qq.com>
2025-01-21 22:42:06 +0800

    feat: update files

--- c++-study-3/day06/demo02/main.cpp
+++ c++-study-3/day06/demo02/main.cpp
@@ -6,6 +6,72 @@
 
 using namespace std;
 
+class A {
+
+public:
+    A(int data) : ma(data) {
+        cout << "A()" << endl;
+    }
+
+    ~A() {
+        cout << "~A()" << endl;
+    }
+
+public:
+    int ma;
+
+};
+
+class B : virtual public A {
+
+public:
+    B(int data) : A(data), mb(data) {
+        cout << "B()" << endl;
+    }
+
+    ~B() {
+        cout << "~B()" << endl;
+    }
+
+public:
+    int mb;
+
+};
+
+class C : virtual public A {
+
+public:
+    C(int data) : A(data), mc(data) {
+        cout << "C()" << endl;
+    }
+
+    ~C() {
+        cout << "~C()" << endl;
+    }
+
+public:
+    int mc;
+
+};
+
+class D : public B, public C {
+
+public:
+    D(int data) : A(data), B(data), C(data), md(data) {
+        cout << "D()" << endl;
+    }
+
+    ~D() {
+        cout << "~D()" << endl;
+    }
+
+public:
+    int md;
+
+};
+
 int main() {
+    D d(20);
+    cout << d.ma << endl;        // 正确写法，不存在二义性
     return 0;
 }
