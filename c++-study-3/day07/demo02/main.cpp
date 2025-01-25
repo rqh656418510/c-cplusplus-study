c1c72f3c000b4d2f34e53ea5671b2193340af18a
rqh656418510 <656418510@qq.com>
2025-01-25 13:43:46 +0800

    Update files

--- c++-study-3/day07/demo02/main.cpp
+++ c++-study-3/day07/demo02/main.cpp
@@ -3,10 +3,51 @@
  */
 
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 int main() {
+    // 设置随机种子
+    srand(time(nullptr));
+
+    // 存储唯一键值（不允许重复）的无序容器
+    unordered_set<int> set1;
+
+    // 插入元素
+    for (int i = 0; i < 50; i++) {
+        set1.insert(random() % 20 + 1);
+    }
+
+    // 遍历容器
+    for (auto it = set1.begin(); it != set1.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    // 获取容器中的元素数量
+    cout << set1.size() << endl;
+
+    // 统计容器中元素是 5 的个数
+    cout << set1.count(5) << endl;
+
+    // 删除元素
+    set1.erase(20);
+
+    // 查找元素
+    auto iterator = set1.find(15);
+    if (iterator != set1.end()) {
+        cout << "find item : " << *iterator << endl;
+    } else {
+        cout << "not find item" << endl;
+    }
+
+    // 循环遍历
+    for (int i: set1) {
+        cout << i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
 
---
+++ c++-study-3/day07/demo02/main2.cpp
@@ -0,0 +1,52 @@
+/**
+ * 无序关联容器
+ */
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    // 设置随机种子
+    srand(time(nullptr));
+
+    // 无序的键值对容器（键不允许重复）
+    unordered_map<int, string> map1;
+
+    // 插入键值对
+    map1.insert(make_pair(1001, "Tom"));
+    map1.insert(make_pair(1002, "Peter"));
+    map1.insert(make_pair(1003, "David"));
+
+    // 获取指定键的值，如果 key 不存在，会自动插入一个键值对 [key, string()]
+    cout << map1[1002] << endl;
+
+    // 插入或者修改操作
+    map1[1004] = "Jim";
+
+    // 获取容器中键值对的数量
+    cout << map1.size() << endl;
+
+    // 删除指定的键
+    map1.erase(1003);
+
+    // 查找
+    auto iterator = map1.find(1001);
+    if (iterator != map1.end()) {
+        // 获取 key 和 value
+        cout << "finded, key: " << iterator->first << ", value: " << iterator->second << endl;
+    } else {
+        cout << "not finded" << endl;
+    }
+
+    // 遍历容器
+    for (auto it = map1.begin(); it != map1.end(); ++it) {
+        // 获取 key 和 value
+        cout << "key: " << it->first << ", value: " << it->second << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
