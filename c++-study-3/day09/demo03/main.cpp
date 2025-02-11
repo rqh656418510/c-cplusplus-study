79c464f6de4b519b08f2df67c8da616821f0a8d1
rqh656418510 <656418510@qq.com>
2025-02-11 17:47:11 +0800

    Update files

--- c++-study-3/day09/demo03/main.cpp
+++ c++-study-3/day09/demo03/main.cpp
@@ -2,6 +2,7 @@
  * CMyString在vector上的应用
  */
 
+
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -12,12 +13,13 @@ class MyString {
 
 public:
     // 构造函数
-    MyString(const char *p = nullptr) {
+    MyString(const char* p = nullptr) {
         cout << "MyString(const char *p = nullptr)" << endl;
         if (p != nullptr) {
             _pstr = new char[strlen(p) + 1];
             strcpy(_pstr, p);
-        } else {
+        }
+        else {
             _pstr = new char[1];
             *_pstr = '\0';
         }
@@ -31,7 +33,7 @@ public:
     }
 
     // 带左值引用参数的拷贝构造函数
-    MyString(const MyString &str) {
+    MyString(const MyString& str) {
         cout << "MyString(const MyString &str)" << endl;
         // 深拷贝
         _pstr = new char[strlen(str._pstr) + 1];
@@ -39,14 +41,15 @@ public:
     }
 
     // 带右值引用参数的拷贝构造函数
-    MyString(MyString &&str) {
-        cout << "MyString(MyString &&str)" << endl;
+    MyString(MyString&& str) {
+        cout << "MyString(MyString&& str)" << endl;
+        // 浅拷贝临时对象（可以提高代码执行效率，减少内存的拷贝次数）
         _pstr = str._pstr;
         str._pstr = nullptr;
     }
 
     // 带左值引用参数的赋值运算符重载
-    MyString &operator=(const MyString &str) {
+    MyString& operator=(const MyString& str) {
         cout << "operator=(const MyString &str)" << endl;
         // 防止自赋值
         if (this == &str) {
@@ -63,65 +66,41 @@ public:
         return *this;
     }
 
+    // 带右值引用参数的赋值运算符重载
+    MyString& operator=(MyString&& str) {
+        cout << "operator=(MyString&& str)" << endl;
+        // 防止自赋值
+        if (this == &str) {
+            return *this;
+        }
+
+        // 释放原来的内存空间
+        delete[] _pstr;
+
+        // 浅拷贝临时对象（可以提高代码执行效率，减少内存的拷贝次数）
+        _pstr = str._pstr;
+        str._pstr = nullptr;
+
+        return *this;
+    }
+
+
     // 加法运算符重载
-    friend MyString operator+(const MyString &str1, const MyString &str2);
+    friend MyString operator+(const MyString& str1, const MyString& str2);
 
     // 左移运算符重载
-    friend ostream &operator<<(ostream &out, const MyString &str);
-
-    // 大于运算符重载
-    bool operator>(const MyString &str) const {
-        return strcmp(_pstr, str._pstr) > 0;
-    }
-
-    // 小于运算符重载
-    bool operator<(const MyString &str) const {
-        return strcmp(_pstr, str._pstr) < 0;
-    }
-
-    // 双等号运算符重载
-    bool operator==(const MyString &str) const {
-        return strcmp(_pstr, str._pstr) == 0;
-    }
-
-    // 中括号运算符重载（读写）
-    char &operator[](int index) {
-        return _pstr[index];
-    }
-
-    // 中括号运算符重载（只读）
-    const char &operator[](int index) const {
-        return _pstr[index];
-    }
+    friend ostream& operator<<(ostream& out, const MyString& str);
 
     // 返回字符串自身
-    const char *c_str() const {
+    const char* c_str() const {
         return _pstr;
     }
 
-    // 获取字符串长度
-    long length() const {
-        long length = strlen(_pstr);
-
-        // 空字符串
-        if (0 == length) {
-            return 0;
-        }
-
-        // 以 '\0' 结尾的字符串
-        if (_pstr[length] == '\0') {
-            return length;
-        }
-
-        // 不以 '\0' 结尾的字符串
-        return length + 1;
-    }
-
 private:
-    char *_pstr;
+    char* _pstr;
 };
 
-MyString operator+(const MyString &str1, const MyString &str2) {
+MyString operator+(const MyString& str1, const MyString& str2) {
     MyString tmpStr;
     tmpStr._pstr = new char[strlen(str1._pstr) + strlen(str2._pstr) + 1];
     strcpy(tmpStr._pstr, str1._pstr);
@@ -129,9 +108,15 @@ MyString operator+(const MyString &str1, const MyString &str2) {
     return tmpStr;
 }
 
-ostream &operator<<(ostream &out, const MyString &str) {
+ostream& operator<<(ostream& out, const MyString& str) {
     out << str._pstr;
     return out;
+}
+
+MyString getString(MyString& str) {
+    const char* pstr = str.c_str();
+    MyString tmpStr(pstr);
+    return tmpStr;
 }
 
 void test01() {
@@ -157,6 +142,6 @@ void test02() {
 
 int main() {
     test01();
-    test02();
+	test02();
     return 0;
 }
