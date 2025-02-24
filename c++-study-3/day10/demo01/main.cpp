2569187c77344b94ef78c18871cee11fedcc7aa7
rqh656418510 <656418510@qq.com>
2025-02-24 19:17:42 +0800

    Update files

--- c++-study-3/day10/demo01/main.cpp
+++ c++-study-3/day10/demo01/main.cpp
@@ -1,11 +1,61 @@
 /**
- * 基础知识
+ * 智能指针基础知识
  */
 
 #include <iostream>
 
 using namespace std;
 
-int main() {
+// 模拟实现智能指针
+template<typename T>
+class CSmartPtr {
 
+public:
+	CSmartPtr(T* ptr) : _ptr(ptr) {
+		cout << "CSmartPtr()" << endl;
+	}
+
+	~CSmartPtr() {
+		cout << "~CSmartPtr()" << endl;
+		delete _ptr;
+	}
+
+	T& operator*() {
+		return *_ptr;
+	}
+
+	T* operator->() {
+		return _ptr;
+	}
+
+private:
+	T* _ptr;
+};
+
+class Person {
+
+public:
+	Person(char* name, int age) : _name(name), _age(age) {
+
+	}
+
+	void print() {
+		cout << "name: " << _name << ", age: " << _age << endl;
+	}
+
+private:
+	char* _name;
+	int _age;
+};
+
+int main() {
+	// 智能指针是利用栈上的对象出作用域后自动析构的特性，来实现资源的自动释放
+	CSmartPtr<int> cptr(new int);
+	*cptr = 30;
+
+	// 错误用法，这样写必须手动执行 delete cptr 来释放资源，即失去智能指针自动释放资源的特性
+	// CSmartPtr<int>* cptr = new CSmartPtr<int>(new int);
+
+	CSmartPtr<Person> cptr2(new Person("Peter", 30));
+	cptr2->print();
 }
