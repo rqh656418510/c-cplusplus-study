1f315c3a05054efcfc9a742422c9cd32225de044
rqh656418510 <656418510@qq.com>
2025-02-24 16:47:55 +0800

    Update files

--- c++-study-3/day09/demo06/main.cpp
+++ c++-study-3/day09/demo06/main.cpp
@@ -6,6 +6,44 @@
 
 using namespace std;
 
+class Test {
+
+public:
+    Test(int a = 10) :_ma(a) {
+        cout << "Test(int a)" << endl;
+    }
+
+    ~Test() {
+        cout << "~Test()" << endl;
+    }
+
+    Test(const Test& t) {
+        cout << "Test(const Test& t)" << endl;
+        this->_ma = t._ma;
+	}
+
+    void operator=(const Test& t) {
+		cout << "operator=(const Test& t)" << endl;
+		this->_ma = t._ma;
+    }
+
+    int getData() {
+        return this->_ma;
+    }
+
+private:
+    int _ma;
+};
+
+Test getObject(Test t) {
+    int value = t.getData();
+    Test tmp(value);
+	return tmp;
+}
+
 int main() {
+    Test t1;
+    Test t2;
+	t2 = getObject(t1);
     return 0;
 }
--- c++-study-3/day09/demo07/main.cpp
+++ c++-study-3/day09/demo07/main.cpp
@@ -6,6 +6,42 @@
 
 using namespace std;
 
+class Test {
+
+public:
+    Test(int a = 10) :_ma(a) {
+        cout << "Test(int a)" << endl;
+    }
+
+    ~Test() {
+        cout << "~Test()" << endl;
+    }
+
+    Test(const Test& t) {
+        cout << "Test(const Test& t)" << endl;
+        this->_ma = t._ma;
+    }
+
+    void operator=(const Test& t) {
+        cout << "operator=(const Test& t)" << endl;
+        this->_ma = t._ma;
+    }
+
+    int getData() {
+        return this->_ma;
+    }
+
+private:
+    int _ma;
+};
+
+Test getObject(Test& t) {
+    int value = t.getData();
+    return Test(value);
+}
+
 int main() {
+    Test t1;
+    Test t2 = getObject(t1);
     return 0;
 }
