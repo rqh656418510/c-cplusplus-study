0038120b9850295c8bd4a4254d363634aafba2fc
rqh656418510 <656418510@qq.com>
2025-03-27 13:10:53 +0800

    feat: update files

--- c++-study-3/day11/demo07/main.cpp
+++ c++-study-3/day11/demo07/main.cpp
@@ -3,9 +3,44 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
 
 using namespace std;
 
 int main() {
+    // 设置随机种子
+    srand(time(nullptr));
+
+    vector<int> vec;
+
+    for (int i = 0; i < 10; ++i) {
+        vec.push_back(rand() % 100 + 1);
+    }
+
+    // 遍历打印容器
+    for_each(vec.begin(), vec.end(), [](int item) -> void { cout << item << " "; });
+    cout << endl;
+
+    // 从大到小排序
+    sort(vec.begin(), vec.end(), [](int a, int b) -> bool { return a > b; });
+
+    // 遍历打印容器
+    for_each(vec.begin(), vec.end(), [](int item) -> void { cout << item << " "; });
+    cout << endl;
+
+    // 查找第一个小于 5 的元素
+    auto it = find_if(vec.begin(), vec.end(), [](int item) -> bool { return item < 50; });
+
+    // 或者简写
+    // auto it = find_if(vec.begin(), vec.end(), [](int item) { return item < 50; });
+
+    if (it != vec.end()) {
+        cout << "找到小于 50 的第一个元素：" << *it << endl;
+    } else {
+        cout << "未找到符合条件的元素" << endl;
+    }
+
     return 0;
 }
--- c++-study-3/day11/demo07/main2.cpp
+++ c++-study-3/day11/demo07/main2.cpp
@@ -3,44 +3,81 @@
  */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <ctime>
+#include <memory>
+#include <map>
+#include <queue>
+#include <functional>
 
 using namespace std;
 
+class Data {
+
+public:
+    Data(int a, int b) : _a(a), _b(b) {
+
+    }
+
+    int getA() {
+        return _a;
+    }
+
+    int getB() {
+        return _b;
+    }
+
+private:
+    int _a;
+    int _b;
+
+};
+
+void test01() {
+    // 使用 function 存储 Lambda 表达式
+    map<int, function<int(int, int)>> map;
+
+    map[0] = [](int a, int b) -> int { return a + b; };
+    map[1] = [](int a, int b) -> int { return a - b; };
+    map[2] = [](int a, int b) -> int { return a * b; };
+    map[3] = [](int a, int b) -> int { return a / b; };
+
+    cout << "30 + 15 = " << map[0](30, 15) << endl;
+    cout << "30 - 15 = " << map[1](30, 15) << endl;
+    cout << "30 * 15 = " << map[2](30, 15) << endl;
+    cout << "30 / 15 = " << map[3](30, 15) << endl;
+}
+
+void test02() {
+    // 智能指针自定义删除器
+    unique_ptr<FILE, function<void(FILE *)>> ptr1(fopen("data.txt", "w"), [](FILE *p) -> void {
+        fclose(p);
+        cout << "Closed File" << endl;
+    });
+}
+
+void test03() {
+    // 优先级队列自定义元素排序规则
+    using FUNC = function<bool(Data &, Data &)>;
+    priority_queue<Data, vector<Data>, FUNC> queue([](Data &data1, Data &data2) {
+        return data1.getA() < data2.getA();
+    });
+    queue.push(Data(10, 20));
+    queue.push(Data(18, 25));
+    queue.push(Data(15, 23));
+    queue.push(Data(19, 28));
+
+    while (!queue.empty()) {
+        Data data = queue.top();
+        queue.pop();
+        cout << "a = " << data.getA() << ", b = " << data.getB() << endl;
+    }
+}
+
 int main() {
-    // 设置随机种子
-    srand(time(nullptr));
-
-    vector<int> vec;
-
-    for (int i = 0; i < 10; ++i) {
-        vec.push_back(rand() % 100 + 1);
-    }
-
-    // 遍历打印容器
-    for_each(vec.begin(), vec.end(), [](int item) -> void { cout << item << " "; });
-    cout << endl;
-
-    // 从大到小排序
-    sort(vec.begin(), vec.end(), [](int a, int b) -> bool { return a > b; });
-
-    // 遍历打印容器
-    for_each(vec.begin(), vec.end(), [](int item) -> void { cout << item << " "; });
-    cout << endl;
-
-    // 查找第一个小于 5 的元素
-    auto it = find_if(vec.begin(), vec.end(), [](int item) -> bool { return item < 50; });
-
-    // 或者简写
-    // auto it = find_if(vec.begin(), vec.end(), [](int item) { return item < 50; });
-
-    if (it != vec.end()) {
-        cout << "找到小于 50 的第一个元素：" << *it << endl;
-    } else {
-        cout << "未找到符合条件的元素" << endl;
-    }
-
+    test01();
+    cout << "----------------" << endl;
+    test02();
+    cout << "----------------" << endl;
+    test03();
+    cout << "----------------" << endl;
     return 0;
 }
