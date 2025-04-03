2eb87a6066051ae9e4c263597cdeacbe9faef47e
rqh656418510 <656418510@qq.com>
2025-04-03 14:38:14 +0800

    feat: update files

--- c++-study-3/day12/demo01/main.cpp
+++ c++-study-3/day12/demo01/main.cpp
@@ -6,16 +6,30 @@
 #include <thread>
 
 void threadHandler1() {
+    // 线程睡眠5秒
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     std::cout << "run thread handler 1" << std::endl;
+}
+
+void threadHandler2(int seconds) {
+    // 线程睡眠
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    std::cout << "run thread handler 2" << std::endl;
 }
 
 int main() {
 
-    // 定义一个线程对象，参数传入一个线程函数
+    // 定义一个线程对象，参数传入一个线程函数，线程会自动启动
     std::thread t1(threadHandler1);
+
+    // 或者指定线程函数的参数
+    // std::thread t1(threadHandler2, 5);
 
     // 主线程等待子线程结束，然后再继续往下执行
     t1.join();
+
+    // 或者将子线程设置为分离线程，这样主线程就不需要等待子线程结束；当主线程结束，所有子线程都自动结束（终止运行）
+    // t1.detach();
 
     return 0;
 }
--- c++-study-3/day12/demo02/main.cpp
+++ c++-study-3/day12/demo02/main.cpp
@@ -1,11 +1,51 @@
-/**
+﻿/**
  * 线程间互斥-mutex互斥锁和lock_guard
+ * 
+ * 模拟车站三个售票窗口同时售票，要求每张票只能卖一次
  */
 
 #include <iostream>
+#include <list>
+#include <thread>
+#include <mutex>
 
-using namespace std;
+// 互斥锁
+std::mutex _mutex;
+
+// 车站一共有 100 张车票
+int ticketCount = 100;
+
+void sellTicket(int id) {
+    while (ticketCount > 0) {
+        // 获取互斥锁（支持在作用域结束时自动解锁）
+        std::lock_guard<std::mutex> lock(_mutex);
+
+        // 双重检测
+        if (ticketCount > 0) {
+            // 执行业务逻辑
+            std::cout << "售票窗口 " << id << " 卖出第 " << ticketCount << " 张车票" << std::endl;
+            ticketCount--;
+        }
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
 
 int main() {
+    const int threadCount = 3;
+    std::list<std::thread> threads;
+
+    // 启动三个售票子线程
+	for (int i = 0; i < threadCount; i++) {
+        threads.push_back(std::thread(sellTicket, i));
+    }
+
+    // 阻塞等待三个售票子线程执行结束
+    for (std::thread& t : threads) {
+        t.join();
+	}
+
+    std::cout << "所有售票窗口已经结束售票" << std::endl;
+
     return 0;
 }
