072b9124f086363887154996db23c70b9aa5f9f0
rqh656418510 <656418510@qq.com>
2025-04-06 20:44:43 +0800

    Update files

---
+++ c++-study-3/day12/demo04/main.cpp
@@ -0,0 +1,9 @@
+/**
+ * 通过thread类编写C++多线程程序
+ */
+
+#include <iostream>
+
+int main() {
+    return 0;
+}
---
+++ c++-study-3/day12/demo05/main.cpp
@@ -0,0 +1,9 @@
+/**
+ * 基于CAS操作的atomic原子类型
+ */
+
+#include <iostream>
+
+int main() {
+    return 0;
+}
