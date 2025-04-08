8a47c7ad53a43e0b4b25c6655bb9c238bf332042
rqh656418510 <656418510@qq.com>
2025-04-08 20:44:44 +0800

    Update files

--- c++-study-3/day13/demo03/main3.cpp
+++ c++-study-3/day13/demo03/main3.cpp
@@ -10,24 +10,33 @@ using namespace std;
 
 class Singleton {
 
-public:
-    static Singleton &getInstance() {
-        static Singleton instance;
-        return instance;
+private:
+    // 私有的默认构造函数
+    Singleton() {
+
     }
 
+    // 删除拷贝构造函数
     Singleton(const Singleton &) = delete;
 
+    // 删除赋值运算符
     Singleton &operator=(const Singleton &) = delete;
 
-private:
-    Singleton() = default;
+public:
+
+    // 获取单例对象
+    static Singleton *getInstance() {
+        // 局部静态变量
+        static Singleton instance;
+
+        return &instance;
+    }
 
 };
 
 int main() {
-    Singleton &s1 = Singleton::getInstance();
-    Singleton &s2 = Singleton::getInstance();
-    cout << (&s1 == &s2 ? "true" : "false") << endl;
+    Singleton *s1 = Singleton::getInstance();
+    Singleton *s2 = Singleton::getInstance();
+    cout << (s1 == s2 ? "true" : "false") << endl;
     return 0;
 }
