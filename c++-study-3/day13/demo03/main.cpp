4cf7c2dd0253540185e78263760793ecf238afab
rqh656418510 <656418510@qq.com>
2025-04-08 18:33:05 +0800

    Update files

--- c++-study-3/day13/demo03/main.cpp
+++ c++-study-3/day13/demo03/main.cpp
@@ -4,7 +4,43 @@
 
 #include <iostream>
 
+using namespace std;
+
+// 饿汉单例模式
+
+class Singleton {
+
+private:
+    // 私有的默认构造函数
+    Singleton() {
+
+    }
+
+    // 删除拷贝造函数
+    Singleton(const Singleton &) = delete;
+
+    // 删除赋值运算符
+    Singleton &operator=(const Singleton &) = delete;
+
+public:
+    // 获取单例
+    static Singleton *getInstance() {
+        return singleton;
+    }
+
+private:
+    // 声明静态变量（单例）
+    static Singleton *singleton;
+
+};
+
+// 初始化静态变量（单例），分配内存空间
+Singleton *Singleton::singleton = new Singleton();
+
 int main() {
+    Singleton *s1 = Singleton::getInstance();
+    Singleton *s2 = Singleton::getInstance();
+    cout << (s1 == s2 ? "true" : "false") << endl;
 
     return 0;
 }
