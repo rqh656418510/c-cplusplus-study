7b0178452e4a7bbe39725a7dec4a7046de17d984
rqh656418510 <656418510@qq.com>
2025-04-30 15:42:57 +0800

    Update files

--- c++-study-3/day10/demo07/main.cpp
+++ c++-study-3/day10/demo07/main.cpp
@@ -3,9 +3,31 @@
  */
 
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
+class Test {
+
+public:
+    Test(int a) : _a(a) {
+        cout << "Test(int)" << endl;
+    }
+
+private:
+    int _a;
+
+};
+
 int main() {
+    shared_ptr<int> sp1(new int(10));
+    shared_ptr<Test> sp2(new Test(20));
+
+    shared_ptr<int> sp3 = make_shared<int>(30);
+    *sp3 = 40;
+    cout << *sp3 << endl;
+
+    shared_ptr<Test> sp4 = make_shared<Test>(50);
+
     return 0;
 }
