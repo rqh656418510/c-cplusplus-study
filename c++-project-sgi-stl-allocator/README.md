7f3c91b0779c76d5c11a4582aea18161de43f4f9
rqh656418510 <656418510@qq.com>
2025-05-10 12:10:48 +0800

    Update files

---
+++ c++-project-sgi-stl-allocator/README.md
@@ -0,0 +1,9 @@
+## SGI STL 内存池源码移植项目
+
+### 项目说明
+
+- 移植 SGI STL 内存池的核心源码。
+
+### 博客教程
+
+- [SGI STL 内存池源码浅析](https://www.techgrow.cn/posts/712a574b.html)
---
+++ c++-project-sgi-stl-allocator/src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "custom_allocator.h"
+
+using namespace std;
+
+int main() {
+    cout << "SGI STL" << endl;
+    return 0;
+}
