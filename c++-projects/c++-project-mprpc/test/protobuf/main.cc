b0c00447056a6f2838dbc92c211d843234ce6a36
rqh656418510 <656418510@qq.com>
2025-06-11 10:12:56 +0000

    Update files

--- c++-projects/c++-project-mprpc/test/protobuf/main.cc
+++ c++-projects/c++-project-mprpc/test/protobuf/main.cc
@@ -2,20 +2,16 @@
 #include <string>
 
 #include "generated/friendservice.pb.h"
+#include "generated/groupservice.pb.h"
 #include "generated/userservice.pb.h"
 
-// 生成代码命令：protoc *.proto --cpp_out=./
-// 编译代码命令：g++ *.cc -o main $(pkg-config --cflags --libs protobuf)
-
 using namespace std;
-using namespace user;
-using namespace friends;
 
 // Protobuf 序列化与反序列化
 void test01() {
     cout << "=============test01=============" << endl;
 
-    LoginRequest req1;
+    user::LoginRequest req1;
     req1.set_name("jim");
     req1.set_pwd("12345");
 
@@ -26,7 +22,7 @@ void test01() {
     }
 
     // C++对象反序列化
-    LoginRequest req2;
+    user::LoginRequest req2;
     if (req2.ParseFromString(serizlize_str)) {
         cout << "name: " << req2.name() << ", pwd: " << req2.pwd() << endl;
     }
@@ -36,23 +32,23 @@ void test01() {
 void test02() {
     cout << "=============test02=============" << endl;
 
-    GetFriendListResponse resp;
+    friends::GetFriendListResponse resp;
 
-    // 设置类成员变量
-    ResultCode *res = resp.mutable_result();
-    res->set_errormsg("server error");
-    res->set_errorno(500);
-    res->set_result(false);
+    // 设置 ResultCode
+    friends::ResultCode *res = resp.mutable_result();
+    res->set_errormsg("success");
+    res->set_errorno(200);
+    res->set_result(true);
 
     // 往列表添加元素
-    User *user1 = resp.add_friendlist();
-    user1->set_sex(User::MAN);
+    friends::User *user1 = resp.add_friendlist();
+    user1->set_sex(friends::User::MAN);
     user1->set_name("jim");
     user1->set_id(1);
 
     // 往列表添加元素
-    User *user2 = resp.add_friendlist();
-    user2->set_sex(User::MAN);
+    friends::User *user2 = resp.add_friendlist();
+    user2->set_sex(friends::User::MAN);
     user2->set_name("tom");
     user2->set_id(2);
 
@@ -62,12 +58,57 @@ void test02() {
     // C++ 对象序列化
     string serialize_str;
     if (!resp.SerializeToString(&serialize_str)) {
-        cerr << "protobuf serialize failed" << endl;
+        cerr << "protobuf list serialize failed" << endl;
+    }
+}
+
+// Protobuf 列表的使用
+void test03() {
+    cout << "=============test03=============" << endl;
+    group::GetGroupResponse resp;
+
+    // 设置 ResultCode
+    group::ResultCode *result = resp.mutable_result();
+    result->set_errormsg("server error");
+    result->set_errorno(500);
+    result->set_result(false);
+
+    // 设置 Group
+    group::Group *group = resp.mutable_group();
+    group->set_id(1001);
+    group->set_groupname("AI Team");
+    group->set_groupdesc("A group for AI");
+
+    // 插入 map 中的 User
+    group::User user1;
+    user1.set_id(1);
+    user1.set_name("Jim");
+    user1.set_sex(group::User::MAN);
+
+    // 插入 map 中的 User
+    group::User user2;
+    user2.set_id(2);
+    user2.set_name("Tom");
+    user2.set_sex(group::User::MAN);
+
+    // 向 map 添加成员（注意：这里使用的是指针引用）
+    (*group->mutable_users())[user1.id()] = user1;
+    (*group->mutable_users())[user2.id()] = user2;
+
+    // 获取 map 的大小
+    cout << "group user map size: " << resp.group().users().size() << endl;
+
+    // C++ 对象序列化
+
+    string serialize_str;
+    if (!resp.SerializeToString(&serialize_str)) {
+        cerr << "protobuf map serialize failed" << endl;
     }
 }
 
 int main() {
     test01();
     test02();
+    test03();
     return 0;
 }
