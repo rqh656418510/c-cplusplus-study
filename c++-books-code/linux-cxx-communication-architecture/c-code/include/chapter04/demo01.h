f5990174f90e3678cd93ea6cbd14763795754cf3
rqh656418510 <656418510@qq.com>
2025-07-01 09:53:51 +0000

    Update files

---
+++ c++-books-code/linux-cxx-communication-architecture/c-code/include/chapter04/demo01.h
@@ -0,0 +1,8 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int chapter04_demo01_fun01() {
+    printf("程序退出!\n");
+    return 0;
+}
--- c++-books-code/linux-cxx-communication-architecture/c-code/src/main.c
+++ c++-books-code/linux-cxx-communication-architecture/c-code/src/main.c
@@ -2,9 +2,11 @@
 
 #include "chapter03/demo01.h"
 #include "chapter03/demo02.h"
+#include "chapter04/demo01.h"
 
 int main() {
-    chapter03_demo01_fun01();
+    // chapter03_demo01_fun01();
     // chapter03_demo02_fun01();
+    chapter04_demo01_fun01();
     return 0;
 }
