93af9cbf5af227ed86e3c868e5a4be47c5d9ab79
rqh656418510 <656418510@qq.com>
2025-07-22 22:20:54 +0800

    Update files

--- c++-study-4/day01/demo03/main.cpp
+++ c++-study-4/day01/demo03/main.cpp
@@ -3,28 +3,46 @@
  */
 
 #include <iostream>
+#include <vector>
 
-void show() {
-	std::cout << "default show()" << std::endl;
+// 范围 for
+void test01() {
+    // 遍历数组
+    int arr[] = {1, 2, 3, 4, 5};
+    for (int item : arr) {
+        std::cout << item << " ";
+    }
+
+    std::cout << std::endl;
+
+    // 遍历容器
+    std::vector<int> v = {6, 7, 8, 9, 10};
+    for (int item : v) {
+        std::cout << item << " ";
+    }
+
+    std::cout << std::endl;
+
+    // 引用遍历（避免拷贝）- 如果元素是大对象，或需要修改元素，建议用引用
+    for (int &item : v) {
+        std::cout << item << " ";
+    }
+
+    std::cout << std::endl;
+
+    // 引用遍历（避免拷贝）- 如果不想修改元素但又不想拷贝，可以用 `const&`
+    for (const int &item : v) {
+        std::cout << item << " ";
+    }
 }
 
-namespace google {
+// 动态内存分配
+void test02() {
 
-	void show() {
-		std::cout << "google show()" << std::endl;
-	}
-
-}
-
-namespace sgi {
-	void show() {
-		std::cout << "sgi show()" << std::endl;
-	}
 }
 
 int main() {
-	google::show();
-	sgi::show();
-	::show();
-	return 0;
+    test01();
+    test02();
+    return 0;
 }
