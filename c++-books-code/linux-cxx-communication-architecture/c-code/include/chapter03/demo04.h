0fa901d27859b16423528297bf3bcfe16345d3d5
rqh656418510 <656418510@qq.com>
2025-07-31 13:38:02 +0000

    Update files

--- c++-books-code/linux-cxx-communication-architecture/c-code/include/chapter03/demo04.h
+++ c++-books-code/linux-cxx-communication-architecture/c-code/include/chapter03/demo04.h
@@ -1,13 +1,14 @@
 /**
- * 使用 signal() 函数来忽略或者捕捉信号
+ * 演示错用 malloc（为不可重入函数）为可重入函数的后果
  */
 
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 // 信号处理函数
-void sig_usr_1(int signo) {
+void sig_usr_2(int signo) {
     if (signo == SIGUSR1) {
         printf("收到了 SIGUSR1 信号!\n");
     } else if (signo == SIGUSR2) {
@@ -17,13 +18,13 @@ void sig_usr_1(int signo) {
     }
 }
 
-// 使用 signal() 函数来忽略或者捕捉信号
-int chapter03_demo03_fun01() {
+// 演示错用 malloc（为不可重入函数）为可重入函数的后果
+int chapter03_demo04_fun01() {
     // 注册信号处理函数
-    if (signal(SIGUSR1, sig_usr_1) == SIG_ERR) {
+    if (signal(SIGUSR1, sig_usr_2) == SIG_ERR) {
         printf("无法注册 SIGUSR1 信号的处理函数\n");
     }
-    if (signal(SIGUSR2, sig_usr_1) == SIG_ERR) {
+    if (signal(SIGUSR2, sig_usr_2) == SIG_ERR) {
         printf("无法注册 SIGUSR2 信号的处理函数\n");
     }
 
