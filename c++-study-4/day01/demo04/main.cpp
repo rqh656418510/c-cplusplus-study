e84eb316f929fca7967a5ef26de571bc05702352
rqh656418510 <656418510@qq.com>
2025-09-04 22:30:00 +0800

    Update files

--- c++-study-4/day01/demo04/main.cpp
+++ c++-study-4/day01/demo04/main.cpp
@@ -12,19 +12,19 @@ struct student {
     char name[50];
 };
 
-// 结构体作为形参（会发生数据拷贝）
+// 结构体作为形参（效率低，会发生数据拷贝）
 void func1(student stu) {
     stu.id = 2;
     strcpy(stu.name, "Bob");
 }
 
-// 结构体作为引用形参（不会发生数据拷贝）
+// 结构体作为引用形参（效率高，不会发生数据拷贝）
 void func2(student &stu) {
     stu.id = 2;
     strcpy(stu.name, "Jim");
 }
 
-// 结构体作为指针形参（不会发生数据拷贝）
+// 结构体作为指针形参（效率高，不会发生数据拷贝）
 void func3(student *stu) {
     stu->id = 2;
     strcpy(stu->name, "Tom");
---
+++ c++-study-4/day01/demo04/main2.cpp
@@ -0,0 +1,12 @@
+/**
+ * 结构体、权限修饰符、类简介
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
