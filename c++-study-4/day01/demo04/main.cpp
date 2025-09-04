4410135368be82b7779514b8a436ef1447ecd8f8
rqh656418510 <656418510@qq.com>
2025-09-04 22:22:28 +0800

    Update files

--- c++-study-4/day01/demo04/main.cpp
+++ c++-study-4/day01/demo04/main.cpp
@@ -1,11 +1,52 @@
 /**
- * 结构、权限修饰符、类简介
+ * 结构体、权限修饰符、类简介
  */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+struct student {
+    int id;
+    char name[50];
+};
+
+// 结构体作为形参（会发生数据拷贝）
+void func1(student stu) {
+    stu.id = 2;
+    strcpy(stu.name, "Bob");
+}
+
+// 结构体作为引用形参（不会发生数据拷贝）
+void func2(student &stu) {
+    stu.id = 2;
+    strcpy(stu.name, "Jim");
+}
+
+// 结构体作为指针形参（不会发生数据拷贝）
+void func3(student *stu) {
+    stu->id = 2;
+    strcpy(stu->name, "Tom");
+}
+
+// 结构体
+void test01() {
+    student student1;
+    student1.id = 1;
+    strcpy(student1.name, "Peter");
+    cout << "id = " << student1.id << ", name = " << student1.name << endl;
+
+    func1(student1);
+    cout << "id = " << student1.id << ", name = " << student1.name << endl;
+
+    func2(student1);
+    cout << "id = " << student1.id << ", name = " << student1.name << endl;
+
+    func3(&student1);
+    cout << "id = " << student1.id << ", name = " << student1.name << endl;
+}
+
 int main() {
-    cout << "Hello World" << endl;
+    test01();
 }
