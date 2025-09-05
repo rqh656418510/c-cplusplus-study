d8cc251173456353dea0f405cb8621777137aaf6
rqh656418510 <656418510@qq.com>
2025-09-05 22:39:23 +0800

    Update files

--- c++-study-4/day01/demo04/main2.cpp
+++ c++-study-4/day01/demo04/main2.cpp
@@ -7,6 +7,38 @@
 
 using namespace std;
 
+class Student {
+
+public:
+    int id;
+    char name[50];
+
+    // 构造函数
+    Student() {
+
+    }
+
+    // 构造函数
+    Student(int id, const char *name) {
+        this->id = id;
+        strcpy(this->name, name);
+    }
+
+    void show() {
+        cout << "id = " << id << ", name = " << name << endl;
+    }
+
+};
+
 int main() {
+    Student student1;
+    student1.id = 1;
+    strcpy(student1.name, "Jim");
+    student1.show();
+
+    const char *name = "Tom";
+    Student student2(2, name);
+    student2.show();
+
     return 0;
 }
