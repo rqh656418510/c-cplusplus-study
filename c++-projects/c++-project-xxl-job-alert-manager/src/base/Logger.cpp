5799fb46331386efb107781b04c60d3dca219ff5
rqh656418510 <656418510@qq.com>
2026-02-12 10:37:30 +0000

    Update files

--- c++-projects/c++-project-xxl-job-alert-manager/src/base/Logger.cpp
+++ c++-projects/c++-project-xxl-job-alert-manager/src/base/Logger.cpp
@@ -71,9 +71,9 @@ Logger::Logger() {
 
             // 获取当前时间
             char time_buf[128] = {0};
-            sprintf(time_buf, "%d-%d-%d %d:%d:%d => %d [%s] ", now_tm->tm_year + 1900, now_tm->tm_mon + 1,
-                    now_tm->tm_mday, now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec, log_thread_id,
-                    log_level_name.c_str());
+            snprintf(time_buf, sizeof(time_buf), "%d-%d-%d %d:%d:%d => %d [%s] ", now_tm->tm_year + 1900,
+                     now_tm->tm_mon + 1, now_tm->tm_mday, now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec,
+                     log_thread_id, log_level_name.c_str());
 
             // 拼接最终的日志内容
             std::ostringstream oss;
