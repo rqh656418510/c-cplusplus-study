a7618a603a42d0edca1da5bd85130074e2a5318b
rqh656418510 <656418510@qq.com>
2026-03-01 16:23:09 +0800

    Update files

---
+++ c++-study-4/day02/demo10/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * RTTI、dynamic_cast、typeid、虚函数表
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ c++-study-4/day02/demo11/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 基类与派生类关系的详细再探讨
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
