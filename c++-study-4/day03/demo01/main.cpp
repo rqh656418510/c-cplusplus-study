a2ee122713c881085c28df3646994dd66b55c884
rqh656418510 <656418510@qq.com>
2026-03-03 21:05:02 +0800

    Update files

--- c++-study-4/day03/demo01/main.cpp
+++ c++-study-4/day03/demo01/main.cpp
@@ -6,6 +6,22 @@
 
 using namespace std;
 
+// 三种形式的引用
+void test01() {
+    // 左值引用（绑定到左值）
+    int value = 10;
+    int &refval = value;
+    refval = 20;
+
+    // 常量引用，也是左值引用（绑定到左值）
+    const int &refval2 = value;
+
+    // 右值引用（绑定到右值）
+    int &&refval3 = 30;
+    refval3 = 35;
+}
+
 int main() {
+    test01();
     return 0;
 }
