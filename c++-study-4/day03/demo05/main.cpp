56971d0b15d00682decb076fc14c7fd1fe99e74a
rqh656418510 <656418510@qq.com>
2026-03-27 22:28:25 +0800

    Update files

--- c++-study-4/day03/demo05/main.cpp
+++ c++-study-4/day03/demo05/main.cpp
@@ -1,11 +1,44 @@
 /**
- * 类型转换构造函数、运算符，类成员指针
+ * 类型转换构造函数、类型转换运算符，类成员指针
+ *
+ * (a) 类型转换构造函数
  */
 
 #include <iostream>
 
 using namespace std;
 
+class MyInt {
+public:
+    // 有参构造函数，同时也称为类型转换构造函数
+    MyInt(int i = 0) : m_num(i) {
+        cout << "MyInt(int)" << endl;
+    }
+
+    ~MyInt() {
+        cout << "~MyInt()" << endl;
+    }
+
+    int getNumber() {
+        return m_num;
+    }
+
+private:
+    int m_num;
+};
+
 int main() {
+    // 这里会发生隐式类型转换，调用的是类型转换构造函数
+    // 编译器用 10 这个数字通过调用 MyInt 类的构造函数创建一个临时 MyInt 对象，并将这个临时对象构造到 i 这个预留空间里面去
+    MyInt i = 10;
+    cout << i.getNumber() << endl;
+
+    // 等效于上面的写法，会发生隐式类型转换，调用的是类型转换构造函数
+    // MyInt i = MyInt(10);
+
+    // 这里不会发生隐式类型转换，调用的是有参构造函数（即类型转换构造函数）
+    MyInt i2(30);
+    cout << i2.getNumber() << endl;
+
     return 0;
 }
---
+++ c++-study-4/day03/demo05/main2.cpp
@@ -0,0 +1,46 @@
+/**
+ * 类型转换构造函数、类型转换运算符，类成员指针
+ *
+ * (a) 类型转换构造运算符
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class MyInt {
+public:
+    // 有参构造函数，同时也称为类型转换构造函数
+    MyInt(int i = 0) : m_num(i) {
+    }
+
+    ~MyInt() {
+    }
+
+    // 类型转换运算符，将类类型转换为其他类型
+    operator int() const {
+        cout << "operator int() const" << endl;
+        return m_num;
+    }
+
+private:
+    int m_num;
+};
+
+int main() {
+    MyInt i(30);
+
+    // 将类类型转换为其他类型（隐式调用类型转换运算符）
+    int i2 = i + 10;
+    cout << i2 << endl;
+
+    // 将类类型转换为其他类型（显式调用类型转换运算符）
+    int i3 = i.operator int() + 15;
+    cout << i3 << endl;
+
+    // 将类类型转换为其他类型（显式调用类型转换运算符）
+    int i4 = static_cast<int>(i) + 25;
+    cout << i4 << endl;
+
+    return 0;
+}
