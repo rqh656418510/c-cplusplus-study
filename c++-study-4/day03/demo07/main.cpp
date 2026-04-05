798faf5e138e9400c34f981cd1e1b628a4d803b4
rqh656418510 <656418510@qq.com>
2026-04-05 22:31:33 +0800

    Update files

--- c++-study-4/day03/demo07/main.cpp
+++ c++-study-4/day03/demo07/main.cpp
@@ -1,11 +1,28 @@
 /**
  * 模板概念，函数模板定义、调用
+ *
+ * (a) 函数模板
  */
 
 #include <iostream>
 
 using namespace std;
 
+// 定义函数模板
+template<typename T>
+T sum(T i1, T i2) {
+    return i1 + i2;
+}
+
+void test01() {
+    int total1 = sum(1, 3);
+    cout << total1 << endl;
+
+    double total2 = sum(3.4, 5.8);
+    cout << total2 << endl;
+}
+
 int main() {
+    test01();
     return 0;
 }
