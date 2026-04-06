976cc89b772671913451f832a6ecef4283b06dbf
rqh656418510 <656418510@qq.com>
2026-04-06 14:10:10 +0800

    Update files

---
+++ c++-study-4/day03/demo08/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * 类模板概念，类模板定义、使用
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ c++-study-4/day03/demo09/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * typename场合、默认模板参数、趣味写法分析
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
