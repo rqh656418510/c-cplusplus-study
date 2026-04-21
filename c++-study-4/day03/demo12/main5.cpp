368d8e7d374fac95f7986dc38d97960184c8fd89
rqh656418510 <656418510@qq.com>
2026-04-21 14:04:34 +0000

    Update files

--- c++-study-4/day03/demo12/main5.cpp
+++ c++-study-4/day03/demo12/main5.cpp
@@ -8,6 +8,33 @@
 
 using namespace std;
 
+// 定义函数模板（泛化版本）
+template <typename T, typename U>
+void func(T t, U u) {
+    cout << "func(T, U) 泛化版本" << endl;
+}
+
+// 定义函数模板（全特化版本）
+template <>
+void func(int t, int u) {
+    cout << "func(int, int) 全特化版本" << endl;
+}
+
+// 定义函数模板（全特化版本）
+template <>
+void func(double t, double u) {
+    cout << "func(double, double) 全特化版本" << endl;
+}
+
 int main() {
+    // 使用泛化版本
+    func(3, 5.6);
+
+    // 使用 <int, int> 全特化版本
+    func(5, 8);
+
+    // 使用 <double, double> 全特化版本
+    func(6.5, 3.5);
+
     return 0;
 }
--- c++-study-4/day03/demo12/main6.cpp
+++ c++-study-4/day03/demo12/main6.cpp
@@ -1,12 +1,26 @@
 /*
  * 模板全特化、偏特化(局部特化)
  *
- * (f) 函数模板全特化
+ * (f) 函数模板 "偏特化"
  */
 
 #include <iostream>
 
 using namespace std;
+
+// 定义函数模板（泛化版本）
+template <typename T, typename U>
+void func(T t, U u) {
+    cout << "func(T, U) 泛化版本" << endl;
+}
+
+// 错误写法，编译不通过，因为函数模板不支持偏特化
+/*
+template <typename U>
+void func<int, U>(int i, U u) {
+    cout << "func(int, U) 偏特化版本" << endl;
+}
+*/
 
 int main() {
     return 0;
