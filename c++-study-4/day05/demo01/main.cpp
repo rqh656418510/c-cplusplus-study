440e8d44ec45a8915e6582c94de5ef5c3255389c
rqh656418510 <656418510@qq.com>
2026-06-07 14:09:36 +0000

    Update files

--- c++-study-4/day05/demo01/main.cpp
+++ c++-study-4/day05/demo01/main.cpp
@@ -7,20 +7,24 @@
 #include <thread>
 
 void func() {
-    for (int i = 1; i <= 1000; ++i) {
+    std::cout << "sub thread start." << std::endl;
+    for (int i = 1; i <= 5; ++i) {
         std::cout << i << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-}
-
-void test01() {
-    // 启动一个普通线程
-    std::thread t(func);
-    // 等待线程执行结束
-    t.join();
+    std::cout << "sub thread end." << std::endl;
 }
 
 int main() {
-    test01();
+    std::cout << "main thread start." << std::endl;
+
+    // 创建并启动一个普通线程
+    std::thread t(func);
+
+    // 阻塞主线程，让主线程等待子线程执行结束，然后再往下执行
+    // 注意：如果这里不让主线程阻塞等待子线程执行结束，会导致程序意外退出（直接崩溃）
+    t.join();
+
+    std::cout << "main thread end." << std::endl;
     return 0;
 }
---
+++ c++-study-4/day05/demo01/main2.cpp
@@ -0,0 +1,10 @@
+/**
+ * 线程启动、结束，创建线程多法、join，detach
+ */
+
+#include <iostream>
+#include <thread>
+
+int main() {
+    return 0;
+}
