7c47a9fdea32dd004beb356ec12881c8002f2bbd
rqh656418510 <656418510@qq.com>
2026-06-09 14:43:50 +0000

    Update files

--- c++-study-4/day05/demo02/main2.cpp
+++ c++-study-4/day05/demo02/main2.cpp
@@ -1,13 +1,15 @@
 /**
  * 线程传参详解，detach()大坑，成员函数做线程函数
  *
- * (a) 传递临时对象作为线程参数
+ * (b) 传递临时对象作为线程参数 - 要避免的陷阱代码一
  */
 
 #include <iostream>
 #include <thread>
 
 // 定义普通函数（带参数）
+// (1) 这里的 i 并不真的是 m_i 的引用，实际上是值传递，即使子线程执行了 detach() 后，在子线程中继续使用 i 也是安全的
+// (2) 这里的 buf 是真正指向 m_buf 的指针，当子线程执行了 detach() 后，在子线程中继续使用 buf 是不安全的（存在未定义行为）
 void func(const int &i, char *buf) {
     std::cout << "sub thread start." << std::endl;
     std::cout << i << std::endl;
@@ -19,15 +21,14 @@ int main() {
     std::cout << "main thread start." << std::endl;
 
     // 局部变量
-    int m_i = 1;
+    int m_i = 100;
     char m_buf[] = "this is a string";
 
     // 创建线程，并传递参数
     std::thread t(func, m_i, m_buf);
 
-    // 阻塞主线程，让主线程等待子线程执行结束，然后再往下执行
-    // 注意：如果这里不让主线程阻塞等待子线程执行结束，会导致程序意外退出（直接崩溃）
-    t.join();
+    // 将主线程与子线程分离（即子线程变为守护线程），主线程无需等待子线程执行完成才继续往下执行
+    t.detach();
 
     std::cout << "main thread end." << std::endl;
     return 0;
---
+++ c++-study-4/day05/demo02/main3.cpp
@@ -0,0 +1,12 @@
+/**
+ * 线程传参详解，detach()大坑，成员函数做线程函数
+ *
+ * (c) 传递临时对象作为线程参数 - 要避免的陷阱代码二
+ */
+
+#include <iostream>
+#include <thread>
+
+int main() {
+    return 0;
+}
