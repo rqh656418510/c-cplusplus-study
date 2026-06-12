e6f16b50ea2ec42bad927b7a8828e578f411ac2a
rqh656418510 <656418510@qq.com>
2026-06-12 14:24:33 +0000

    Update files

--- c++-study-4/day05/demo02/main6.cpp
+++ c++-study-4/day05/demo02/main6.cpp
@@ -1,10 +1,11 @@
 /**
  * 线程传参详解，detach()大坑，成员函数做线程函数
  *
- * (e) 传递类对象、智能指针作为线程参数 - std::ref() 使用
+ * (f) 传递类对象、智能指针作为线程参数 - 传递智能指针
  */
 
 #include <iostream>
+#include <memory>
 #include <thread>
 
 class MyClass {
@@ -29,10 +30,10 @@ public:
 };
 
 // 定义普通函数（带参数）
-void func(MyClass &mc) {
+void func(std::unique_ptr<MyClass> up) {
     std::cout << "sub thread start." << std::endl;
-    // 主线程中使用 std::ref() 强制传递引用后，这里修改的是主线程中的 m_c 对象
-    mc.m_i = 300;
+    // 主线程中传递智能指针后，这里修改的是主线程中的 m_c 对象
+    up->m_i = 300;
     std::cout << "sub thread end." << std::endl;
 }
 
@@ -40,10 +41,10 @@ int main() {
     std::cout << "main thread start." << std::endl;
 
     // 局部变量
-    MyClass m_c(200);
+    std::unique_ptr<MyClass> m_c(new MyClass(200));
 
-    // 在创建子线程时，使用 std::ref() 强制传递引用
-    std::thread t(func, std::ref(m_c));
+    // 在创建子线程时，传递智能指针
+    std::thread t(func, std::move(m_c));
 
     // 主线程需等待子线程执行完成才继续往下执行
     t.join();
---
+++ c++-study-4/day05/demo02/main7.cpp
@@ -0,0 +1,60 @@
+/**
+ * 线程传参详解，detach()大坑，成员函数做线程函数
+ *
+ * (g) 使用成员函数做线程函数
+ */
+
+#include <iostream>
+#include <thread>
+
+class MyClass {
+public:
+    // 有参构造函数
+    MyClass(int i) : m_i(i) {
+        std::cout << "MyClass()" << std::endl;
+    }
+
+    // 拷贝构造函数
+    MyClass(const MyClass &mc) : m_i(mc.m_i) {
+        std::cout << "MyClass(const MyClass &)" << std::endl;
+    }
+
+    // 析构函数
+    ~MyClass() {
+        std::cout << "~MyClass()" << std::endl;
+    }
+
+    // 成员函数
+    void thread_run(MyClass &mc) {
+        std::cout << "sub thread start." << std::endl;
+        // 主线程中使用 std::ref() 强制传递引用后，这里修改的是主线程中的 m_c 对象
+        mc.m_i = 300;
+        std::cout << "sub thread end." << std::endl;
+    }
+
+public:
+    int m_i;
+};
+
+int main() {
+    std::cout << "main thread start." << std::endl;
+
+    // 局部变量
+    MyClass m_c(200);
+
+    // 在创建子线程时，使用 std::ref() 强制传递引用给线程函数
+    // 第一个参数是成员函数指针，第二个参数是类对象（可以是指针或者引用），第三个参数是真正传递给线程函数的参数
+    std::thread t(&MyClass::thread_run, &m_c, std::ref(m_c));
+
+    // 等价于上面的写法
+    // std::thread t(&MyClass::thread_run, std::ref(m_c), std::ref(m_c));
+
+    // 注意：如果使用下面这种写法（合法），又会多拷贝一份 m_c 对象，导致线程函数里修改的不是主线程中的 m_c 对象
+    // std::thread t(&MyClass::thread_run, m_c, std::ref(m_c));
+
+    // 主线程需等待子线程执行完成才继续往下执行
+    t.join();
+
+    std::cout << "main thread end." << std::endl;
+    return 0;
+}
