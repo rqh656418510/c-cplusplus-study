cf79cd68ef83c7c7d2ade30f92f9919304b8d2b4
rqh656418510 <656418510@qq.com>
2026-06-13 15:01:12 +0000

    Update files

--- c++-study-4/day05/demo03/main.cpp
+++ c++-study-4/day05/demo03/main.cpp
@@ -1,11 +1,35 @@
 /**
  * 创建多个线程、数据共享问题分析、案例代码
+ *
+ * (a) 创建和等待多个线程
  */
 
+#include <chrono>
 #include <iostream>
+#include <thread>
+#include <vector>
 
-using namespace std;
+void func(int num) {
+    std::cout << "sub thread " << num << " start." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout << "sub thread " << num << " end." << std::endl;
+}
 
 int main() {
+    std::cout << "main thread start." << std::endl;
+
+    std::vector<std::thread> threads;
+
+    // 创建 10 个线程，同时这 10 个线程会立即执行（多个线程之前的启动顺序不确定）
+    for (int i = 0; i < 10; i++) {
+        threads.emplace_back(std::thread(func, i));
+    }
+
+    // 等待所有线程执行完毕
+    for (std::vector<std::thread>::iterator it = threads.begin(); it != threads.end(); ++it) {
+        it->join();
+    }
+
+    std::cout << "main thread end." << std::endl;
     return 0;
 }
--- c++-study-4/day05/demo03/main2.cpp
+++ c++-study-4/day05/demo03/main2.cpp
@@ -1,10 +1,11 @@
 /**
  * 创建多个线程、数据共享问题分析、案例代码
+ *
+ * (b) 数据共享问题分析
  */
 
 #include <iostream>
-
-using namespace std;
+#include <thread>
 
 int main() {
     return 0;
