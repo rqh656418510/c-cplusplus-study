17ce87856759bd87157ca40149adec1da42359b7
rqh656418510 <656418510@qq.com>
2026-06-14 13:57:36 +0000

    Update files

--- c++-study-4/day05/demo03/main2.cpp
+++ c++-study-4/day05/demo03/main2.cpp
@@ -1,12 +1,39 @@
 /**
  * 创建多个线程、数据共享问题分析、案例代码
  *
- * (b) 数据共享问题分析
+ * (b) 数据共享问题分析 - 只读的数据
  */
 
+#include <chrono>
 #include <iostream>
 #include <thread>
+#include <vector>
+
+// 共享数据
+std::vector<int> g_v = {1, 2, 3};
+
+void func(int num) {
+    std::cout << "sub thread " << num << " start." << std::endl;
+    std::cout << "sub thread " << num << " read " << g_v[0] << ", " << g_v[1] << ", " << g_v[2] << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::cout << "sub thread " << num << " end." << std::endl;
+}
 
 int main() {
+    std::cout << "main thread start." << std::endl;
+
+    std::vector<std::thread> threads;
+
+    // 创建 5 个线程，同时这 5 个线程会立即执行（多个线程之间的启动顺序是不确定的）
+    for (int i = 0; i < 5; i++) {
+        threads.emplace_back(std::thread(func, i));
+    }
+
+    // 等待所有线程执行完毕
+    for (std::vector<std::thread>::iterator it = threads.begin(); it != threads.end(); ++it) {
+        it->join();
+    }
+
+    std::cout << "main thread end." << std::endl;
     return 0;
 }
---
+++ c++-study-4/day05/demo03/main4.cpp
@@ -0,0 +1,58 @@
+/**
+ * 创建多个线程、数据共享问题分析、案例代码
+ *
+ * (d) 共享数据的保护
+ */
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+// 共享数据（有读有写）
+std::vector<int> g_v = {1, 2, 3};
+
+void func(int num) {
+    std::cout << "sub thread " << num << " start." << std::endl;
+
+    // 前两个线程：执行写操作
+    if (num < 2) {
+        // 写操作：添加一个新元素
+        g_v.push_back(num);
+        std::cout << "sub thread " << num << " write value " << num << std::endl;
+    }
+    // 后三个线程：执行读操作
+    else {
+        // 读操作：读取 vector 的所有元素
+        std::cout << "sub thread " << num << " reads: ";
+        for (int val : g_v) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // 模拟一些工作耗时
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    std::cout << "sub thread " << num << " end." << std::endl;
+}
+
+int main() {
+    std::cout << "main thread start." << std::endl;
+
+    std::vector<std::thread> threads;
+
+    // 创建 5 个线程，其中 2 个线程执行写操作，3 个线程执行读操作
+    // 由于读写同时发生，可能会导致未定义行为（比如读取到不一致的值、程序崩溃等）
+    for (int i = 0; i < 5; i++) {
+        threads.emplace_back(func, i);
+    }
+
+    // 等待所有线程执行完毕
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    std::cout << "main thread end." << std::endl;
+    return 0;
+}
