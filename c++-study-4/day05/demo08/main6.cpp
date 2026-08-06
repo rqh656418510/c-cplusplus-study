8baa338258890a4b54df70f9621cbe19749e90d2
rqh656418510 <656418510@qq.com>
2026-08-06 16:41:47 +0800

    Update files

---
+++ c++-study-4/day05/demo08/main6.cpp
@@ -0,0 +1,39 @@
+/**
+ * async、future、packaged_task 使用
+ *
+ * (f) - `std::packaged_task` + Lambda 表达式的使用
+ */
+
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
+
+int main() {
+    std::cout << "main() run, thread id " << std::this_thread::get_id() << std::endl;
+
+    // 创建 packaged_task，参数是可调用对象（比如 Lambda 表达式）
+    std::packaged_task<int(int)> task([](const int milliseconds) -> int {
+        std::cout << "process start, current thread id " << std::this_thread::get_id() << std::endl;
+        // 模拟业务处理耗时
+        const std::chrono::milliseconds ms(milliseconds);
+        std::this_thread::sleep_for(ms);
+        std::cout << "process end, current thread id " << std::this_thread::get_id() << std::endl;
+        // 返回业务处理结果
+        return 5;
+    });
+
+    // 创建子线程（第二个参数是线程函数的参数），子线程会直接执行
+    std::thread t1(std::ref(task), 5000);
+
+    // 等待子线程执行完成
+    t1.join();
+
+    // 获取子线程的执行结果
+    std::future<int> result = task.get_future();
+    const int num = result.get();
+    std::cout << "result = " << num << std::endl;
+
+    std::cout << "main() end, thread id " << std::this_thread::get_id() << std::endl;
+    return 0;
+}
