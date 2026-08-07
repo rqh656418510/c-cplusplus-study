23a8f99d9bfce8d3c194cdd89fc8024350059170
rqh656418510 <656418510@qq.com>
2026-08-07 17:41:49 +0800

    Update fies

--- c++-study-4/day05/demo08/main11.cpp
+++ c++-study-4/day05/demo08/main11.cpp
@@ -1,7 +1,7 @@
 /**
  * async、future、packaged_task 使用
  *
- * (j) - `std::promise` 的使用
+ * std::promise 的使用
  */
 
 #include <chrono>
@@ -22,24 +22,35 @@ void process(std::promise<int>& result, int milliseconds) {
     result.set_value(5);
 }
 
+void process2(std::future<int> result) {
+    std::cout << "process2() start, current thread id " << std::this_thread::get_id() << std::endl;
+
+    // 阻塞等待任务执行结果
+    const int num = result.get();
+    std::cout << "result = " << num << std::endl;
+
+    std::cout << "process2() end, current thread id " << std::this_thread::get_id() << std::endl;
+}
+
 int main() {
     std::cout << "main() run, thread id " << std::this_thread::get_id() << std::endl;
 
     // 创建 Promise 对象
     std::promise<int> mpro;
 
-    // 获取子线程的执行结果，特别注意：get_future() 应该在子线程执行前调用
+    // 获取执行结果，特别注意：get_future() 应该在子线程执行前调用
     std::future<int> result = mpro.get_future();
 
-    // 创建子线程，第一个参数是线程函数，第二个参数是 Promise 对象的引用，第三个参数毫秒数
+    // 创建子线程 1，第一个参数是线程函数，第二个参数是 Promise 对象的引用，第三个参数毫秒数
     std::thread t1(process, std::ref(mpro), 5000);
+
+    // 创建子线程 2，第一个参数是线程函数，第二个参数是 Future 对象
+    // future 不支持拷贝，通过移动转移所有权
+    std::thread t2(process2, std::move(result));
 
     // 阻塞等待子线程执行完成
     t1.join();
-
-    // 获取子线程的执行结果
-    const int num = result.get();
-    std::cout << "result = " << num << std::endl;
+    t2.join();
 
     std::cout << "main() end, thread id " << std::this_thread::get_id() << std::endl;
     return 0;
