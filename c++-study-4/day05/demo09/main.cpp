816c9294067c9dc0ab8e14cbb4b753d4aab9bb98
rqh656418510 <656418510@qq.com>
2026-08-08 17:37:44 +0800

    Update files

--- c++-study-4/day05/demo09/main.cpp
+++ c++-study-4/day05/demo09/main.cpp
@@ -1,11 +1,45 @@
 /**
  * future 其他成员函数、shared_future、ato 使用
+ *
+ * （a) std::future::wait_for() 的使用
  */
 
+#include <chrono>
+#include <future>
 #include <iostream>
+#include <thread>
 
-using namespace std;
+int process(int milliseconds) {
+    std::cout << "process() start, current thread id " << std::this_thread::get_id() << std::endl;
+    // 模拟业务处理耗时
+    const std::chrono::milliseconds ms(milliseconds);
+    std::this_thread::sleep_for(ms);
+    std::cout << "process() end, current thread id " << std::this_thread::get_id() << std::endl;
+    // 返回业务处理结果
+    return 5;
+}
 
 int main() {
+    std::cout << "main() run, thread id " << std::this_thread::get_id() << std::endl;
+    // 启动一个子线程，第二个参数是线程函数的参数
+    std::future<int> result = std::async(std::launch::async, process, 5000);
+    std::cout << "continue ..." << std::endl;
+
+    // 等待指定时间
+    std::future_status status =  result.wait_for(std::chrono::seconds(2));
+
+    // 判断 Future 的状态（任务执行状态）
+    if (status == std::future_status::timeout) {
+        std::cout << "future timeout" << std::endl;
+    }
+    else if (status == std::future_status::ready) {
+        std::cout << "future ready" << std::endl;
+    }else if (status == std::future_status::deferred) {
+        std::cout << "future deferred" << std::endl;
+    } else {
+        std::cout << "future unknow status" << std::endl;
+    }
+
+    std::cout << "main() end, thread id " << std::this_thread::get_id() << std::endl;
     return 0;
 }
---
+++ c++-study-4/day05/demo09/main2.cpp
@@ -0,0 +1,48 @@
+﻿/**
+* future 其他成员函数、shared_future、ato 使用
+ *
+ * （b) std::shared_future 的使用
+ */
+
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
+
+int process(const int milliseconds) {
+    std::cout << "process() start, current thread id " << std::this_thread::get_id() << std::endl;
+
+    // 模拟业务处理耗时
+    const std::chrono::milliseconds ms(milliseconds);
+    std::this_thread::sleep_for(ms);
+
+    std::cout << "process() end, current thread id " << std::this_thread::get_id() << std::endl;
+
+    // 返回业务处理结果
+    return 5;
+}
+
+int main() {
+    std::cout << "main() run, thread id " << std::this_thread::get_id() << std::endl;
+
+    // 创建 packaged_task，参数是可调用对象（比如普通函数）
+    std::packaged_task<int(int)> task(process);
+
+    // 获取子线程的执行结果，特别注意：get_future() 必须在 packaged_task 执行前调用
+    std::future<int> result = task.get_future();
+
+    // 创建子线程（第二个参数是线程函数的参数），子线程会直接执行
+    std::thread t1(std::ref(task), 5000);
+
+    // 等待子线程执行完成
+    t1.join();
+
+    // 获取执行结果
+    const int num = result.get();
+
+    std::cout << "result = " << num << std::endl;
+
+    std::cout << "main() end, thread id " << std::this_thread::get_id() << std::endl;
+
+    return 0;
+}
