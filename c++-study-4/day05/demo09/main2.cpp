a754df7f3be79024c2dc4dd3dc4485e2abb04396
rqh656418510 <656418510@qq.com>
2026-09-01 21:55:25 +0800

    Update files

--- c++-study-4/day05/demo09/main2.cpp
+++ c++-study-4/day05/demo09/main2.cpp
@@ -22,6 +22,15 @@ int process(const int milliseconds) {
     return 5;
 }
 
+void process2(const std::shared_future<int> &result) {
+    std::cout << "process2() start, current thread id " << std::this_thread::get_id() << std::endl;
+
+    // 获取其他线程的执行结果
+    std::cout << "result = " << result.get() << std::endl;
+
+    std::cout << "process2() end, current thread id " << std::this_thread::get_id() << std::endl;
+}
+
 int main() {
     std::cout << "main() run, thread id " << std::this_thread::get_id() << std::endl;
 
@@ -31,15 +40,24 @@ int main() {
     // 获取子线程的执行结果，特别注意：get_future() 必须在 packaged_task 执行前调用
     std::future<int> result = task.get_future();
 
-    // 创建子线程（第二个参数是线程函数的参数），子线程会直接执行
+    // 将 future 转换为 shared_future
+    // std::shared_future<int> result2(std::move(result));
+    std::shared_future<int> result2(result.share());
+
+    // 创建子线程 1（第二个参数是线程函数的参数），子线程会直接执行
     std::thread t1(std::ref(task), 5000);
 
-    // 等待子线程执行完成
+    // 创建子线程 2（第二个参数是线程函数的参数），子线程会直接执行
+    std::thread t2(process2, std::ref(result2));
+
+    // 等待子线程 1 执行完成
     t1.join();
 
-    // 获取执行结果
-    const int num = result.get();
+    // 等待子线程 2 执行完成
+    t2.join();
 
+    // 获取其他线程的执行结果
+    const int num = result2.get();
     std::cout << "result = " << num << std::endl;
 
     std::cout << "main() end, thread id " << std::this_thread::get_id() << std::endl;
---
+++ c++-study-4/day05/demo09/main3.cpp
@@ -0,0 +1,40 @@
+/**
+* future 其他成员函数、shared_future、ato 使用
+ *
+ * （c) std::atomic 的使用
+ */
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+// 定义全局变量
+static std::atomic<int> g_count(0);
+
+void process()
+{
+    for (int i = 0; i < 1000000; ++i)
+    {
+        g_count++;
+    }
+}
+
+int main()
+{
+    // 创建线程 1
+    std::thread t1(process);
+
+    // 创建线程 2
+    std::thread t2(process);
+
+    // 等待线程 1 执行完成
+    t1.join();
+
+    // 等待线程 2 执行完成
+    t2.join();
+
+    // 预期输出结果是：2000000
+    std::cout << "count = " << g_count.load() << std::endl;
+
+    return 0;
+}
