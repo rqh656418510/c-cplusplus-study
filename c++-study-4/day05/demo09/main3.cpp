f8b250e8fc8f9822caf015653e8e72b53b387706
rqh656418510 <656418510@qq.com>
2026-09-02 13:32:46 +0800

    Update files

--- c++-study-4/day05/demo09/main3.cpp
+++ c++-study-4/day05/demo09/main3.cpp
@@ -1,26 +1,23 @@
 /**
-* future 其他成员函数、shared_future、ato 使用
+ * future 其他成员函数、shared_future、ato 使用
  *
  * （c) std::atomic 的使用
  */
 
+#include <atomic>
 #include <iostream>
 #include <thread>
-#include <atomic>
 
 // 定义全局变量（使用原子类型）
 static std::atomic<int> g_count(0);
 
-void process()
-{
-    for (int i = 0; i < 1000000; ++i)
-    {
+void process() {
+    for (int i = 0; i < 1000000; ++i) {
         g_count++;
     }
 }
 
-int main()
-{
+int main() {
     // 创建子线程 1
     std::thread t1(process);
 
