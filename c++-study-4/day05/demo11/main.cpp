c34866db2e51a48a3513c1c3fd71d04d182adda6
rqh656418510 <656418510@qq.com>
2026-09-04 22:41:28 +0800

    Update files

--- c++-study-4/day05/demo11/main.cpp
+++ c++-study-4/day05/demo11/main.cpp
@@ -1,12 +1,149 @@
 /**
-* windows临界区、其他各种mutex互斥量
+ * windows临界区、其他各种mutex互斥量
  *
- * （a) windows临界区
+ * （a) windows 临界区使用
  */
 
+#include <atomic>
 #include <iostream>
+#include <list>
+#include <mutex>
+#include <thread>
+#include <Windows.h>
+
+#define __WINDOWSJQ_  // 标记 Windows 系统环境
+
+class MyClass {
+public:
+    // 将收到的玩家命令写入队列
+    void inMsgRecvQueue() {
+        for (int i = 0; i < 1000; ++i) {
+            #ifdef __WINDOWSJQ_
+                // 加锁
+                EnterCriticalSection(&winsec);
+
+                // 插入队列
+                msgRecvQueue.push_back(i);
+
+                // 解锁
+                LeaveCriticalSection(&winsec);
+            #else
+                {
+                    // 加锁（出了作用域后会自动解锁）
+                    std::unique_lock<std::mutex> lock(msgRecvQueueMutex);
+
+                    // 插入队列
+                    msgRecvQueue.push_back(i);
+                }
+            #endif
+
+            // 模拟网络收包间隔
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+
+        // 更新程序停止标记
+        stop = true;
+    }
+
+    // 从队列中读取玩家命令
+    void outMsgRecvQueue() {
+        while (true) {
+            int command = -1;
+
+            #ifdef __WINDOWSJQ_
+                // 加锁
+                EnterCriticalSection(&winsec);
+
+                // 判断程序停止标记
+                if (stop && msgRecvQueue.empty()) {
+                    // 解锁
+                    LeaveCriticalSection(&winsec);
+                    break;
+                }
+
+                // 操作队列
+                if (!msgRecvQueue.empty()) {
+                    // 取出队列元素
+                    command = msgRecvQueue.front();
+                    // 移除队列元素
+                    msgRecvQueue.pop_front();
+                }
+
+                // 解锁
+                LeaveCriticalSection(&winsec);
+            #else 
+                {
+                    // 加锁（出了作用域后会自动解锁）
+                    std::unique_lock<std::mutex> lock(msgRecvQueueMutex);
+
+                    // 判断程序停止标记
+                    if (stop && msgRecvQueue.empty()) {
+                        break;
+                    }
+
+                    // 操作队列
+                    if (!msgRecvQueue.empty()) {
+                        // 取出队列元素
+                        command = msgRecvQueue.front();
+                        // 移除队列元素
+                        msgRecvQueue.pop_front();
+                    }
+                }
+            #endif
+
+            // 打印
+            if (command != -1) {
+                std::cout << "已处理玩家命令: " << command << std::endl;
+            }
+
+            // 模拟业务执行耗时
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    }
+
+    // 构造函数
+    MyClass() {
+        #ifdef __WINDOWSJQ_
+            InitializeCriticalSection(&winsec);  // 初始化临界区
+        #endif
+    }
+
+    // 析构函数
+    ~MyClass() {
+        #ifdef __WINDOWSJQ_
+            DeleteCriticalSection(&winsec);  // 删除临界区
+        #endif
+    }
+
+private:
+    std::list<int> msgRecvQueue;   // 消息队列（共享数据）
+    std::mutex msgRecvQueueMutex;  // 保护消息队列线程安全的互斥锁
+    std::atomic_bool stop{false};  // 程序停止标记
+
+    #ifdef __WINDOWSJQ_
+        CRITICAL_SECTION winsec;  // Windows 系统中的临界区，作用非常类似于 C++ 11 中的 std::mutex
+    #endif
+    
+};
 
 int main() {
+    std::cout << "main thread start." << std::endl;
 
+    // 局部变量
+    MyClass mc;
+
+    // 创建并启动写线程
+    std::thread t_write(&MyClass::inMsgRecvQueue, &mc);
+
+    // 创建并启动读线程
+    std::thread t_read(&MyClass::outMsgRecvQueue, &mc);
+
+    // 等待写线程执行完毕
+    t_write.join();
+
+    // 等待读线程执行完毕
+    t_read.join();
+
+    std::cout << "main thread end." << std::endl;
     return 0;
 }
--- c++-study-4/day05/demo11/main2.cpp
+++ c++-study-4/day05/demo11/main2.cpp
@@ -1,7 +1,7 @@
 /**
 * windows临界区、其他各种mutex互斥量
  *
- * （b) 其他各种mutex互斥量
+ * （b) 多次进入 windows 临界区
  */
 
 #include <iostream>
