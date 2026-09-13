c6b241e30433c53859e37610b9a1e4da6ec5e305
rqh656418510 <656418510@qq.com>
2026-09-13 22:11:23 +0800

    Update files

--- c++-study-4/day06/demo01/main.cpp
+++ c++-study-4/day06/demo01/main.cpp
@@ -4,6 +4,21 @@
 
 #include <iostream>
 
+class MyTest {
+
+public:
+    MyTest() {
+
+    }
+
+private:
+    int m_i;
+};
+
 int main() {
+    // 加括号 ()，成员变量 m_i 的值为随机数
+    MyTest* mt = new MyTest();
+    // 不加括号 ()，成员变量 m_i 的值为随机数
+    MyTest* mt2 = new MyTest;
     return 0;
 }
