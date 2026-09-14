4b5ac1111908ceb956a150fc485651f9c5ee82f5
rqh656418510 <656418510@qq.com>
2026-09-14 21:19:47 +0800

    Update files

--- c++-study-4/day06/demo02/main.cpp
+++ c++-study-4/day06/demo02/main.cpp
@@ -1,9 +1,22 @@
 /**
  * new细节探秘，重载类operator new delete
+ *
+ * (a) new 分配内存的细节
  */
 
+#include <cstring>
 #include <iostream>
 
+void fun() {
+    char* ptr = new char[10];
+    memset(ptr, '\0', 10);
+    delete[] ptr;
+
+    int* ptr2 = new int(10);
+    delete ptr2;
+}
+
 int main() {
+    fun();
     return 0;
 }
