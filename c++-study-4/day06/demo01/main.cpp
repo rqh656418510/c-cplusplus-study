7137097548b631d52f1c44ebf89f2a232627accd
rqh656418510 <656418510@qq.com>
2026-09-14 17:35:58 +0800

    Update files

--- c++-study-4/day06/demo01/main.cpp
+++ c++-study-4/day06/demo01/main.cpp
@@ -1,24 +1,17 @@
 /**
  * new、delete的进一步认识
+ *
+ * (a) 如果是一个空类，使用 `new` 创建对象时，加括号与不加括号没有任何区别
  */
 
 #include <iostream>
 
-class MyTest {
-
-public:
-    MyTest() {
-
-    }
-
-private:
-    int m_i;
-};
+class MyTest {};
 
 int main() {
-    // 加括号 ()，成员变量 m_i 的值为随机数
+    // 加括号 ()
     MyTest* mt = new MyTest();
-    // 不加括号 ()，成员变量 m_i 的值为随机数
+    // 不加括号 ()
     MyTest* mt2 = new MyTest;
     return 0;
 }
--- c++-study-4/day06/demo01/main3.cpp
+++ c++-study-4/day06/demo01/main3.cpp
@@ -1,18 +1,23 @@
 /**
  * new、delete的进一步认识
  *
- * (b) 如果类中有成员变量，带括号的 `new` 会将一些和成员变量相关的内存置零，但不是整个对象的内存全部置零
+ * (c) 如果类中有成员变量，且有自定义的空构造函数，两种写法没有任何区别
  */
 
 #include <iostream>
 
 class MyTest {
+public:
+    // 空构造函数
+    MyTest() {
+    }
+
 private:
     int m_i;  // 成员变量
 };
 
 int main() {
-    // 加括号 ()，成员变量 m_i 的值为 0
+    // 加括号 ()，成员变量 m_i 的值为随机数
     MyTest* mt = new MyTest();
     // 不加括号 ()，成员变量 m_i 的值为随机数
     MyTest* mt2 = new MyTest;
