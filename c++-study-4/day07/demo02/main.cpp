6383452da5b9d283323eb6e76a3afd76d7f80436
rqh656418510 <656418510@qq.com>
2026-09-20 13:44:10 +0800

    Update files

--- c++-study-4/day07/demo02/main.cpp
+++ c++-study-4/day07/demo02/main.cpp
@@ -1,11 +1,44 @@
 /**
  * 容器的说明和简单应用例续
+ *
+ * (a) deque 容器使用
  */
 
+#include <deque>
 #include <iostream>
 
 using namespace std;
 
+class MyClass {
+public:
+    MyClass() {
+        std::cout << "MyClass()" << std::endl;
+    }
+
+    MyClass(const MyClass& obj) {
+        std::cout << "MyClass(const MyClass & obj)" << std::endl;
+    }
+
+    ~MyClass() {
+        std::cout << "~MyClass()" << std::endl;
+    }
+
+private:
+    int m_i = 0;
+};
+
 int main() {
+    deque<MyClass> deq;
+
+    for (int i = 0; i < 3; ++i) {
+        cout << "---------- begin ----------" << endl;
+        deq.emplace_back();
+        cout << "---------- end ------------" << endl;
+    }
+
+    for (int i = 0; i < deq.size(); ++i) {
+        printf("对象 deq[%d] 的地址：0x%llx\n", i, &deq[i]);
+    }
+
     return 0;
 }
