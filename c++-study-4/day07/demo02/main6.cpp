5de13ddfc570e6d415a3a07c37811fc0505b9a9d
rqh656418510 <656418510@qq.com>
2026-09-21 16:54:21 +0800

    Update files

--- c++-study-4/day07/demo02/main6.cpp
+++ c++-study-4/day07/demo02/main6.cpp
@@ -5,9 +5,24 @@
  */
 
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main() {
+    map<int, string> myMap;
+
+    myMap.insert(make_pair(1, "Tom"));
+    myMap.insert(make_pair(2, "Jim"));
+    myMap.insert(pair<int, string>(3, "Peter"));
+
+    // 当用 insert 插入已存在的 Key，会插入失败，原值不变
+    myMap.insert(pair<int, string>(1, "David"));
+
+    auto iter = myMap.find(1);
+    if (iter != myMap.end()) {
+        cout << "key: " << iter->first << ", value: " << iter->second << endl;
+    }
+
     return 0;
 }
--- c++-study-4/day07/demo02/main7.cpp
+++ c++-study-4/day07/demo02/main7.cpp
@@ -1,13 +1,38 @@
 /**
  * 容器的说明和简单应用例续
  *
- * (g) set 使用
+ * (g) unordered_set 使用
  */
 
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 int main() {
+    unordered_set<int> mySet;
+    cout << "bucket count: " << mySet.bucket_count() << endl;
+    cout << "bucket size: " << mySet.bucket_size(0) << endl;
+    cout << "size: " << mySet.size() << endl;
+
+    for (int i = 0; i < 5; ++i) {
+        mySet.insert(i);
+    }
+
+    cout << "bucket count: " << mySet.bucket_count() << endl;
+    cout << "bucket size: " << mySet.bucket_size(0) << endl;
+    cout << "size: " << mySet.size() << endl;
+
+    for (int i = 0; i < mySet.bucket_count(); ++i) {
+        cout << "第 " << i + 1 << " 个 bucket 有 " << mySet.bucket_size(i) << " 元素" << endl;
+    }
+
+    const auto iter = mySet.find(3);
+    if (iter != mySet.end()) {
+        cout << "found number " << *iter << endl;
+    } else {
+        cout << "not found number 3" << endl;
+    }
+
     return 0;
 }
