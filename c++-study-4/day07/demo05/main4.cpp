6742ecdcac4022e82d120143aeac36e7763f79ad
rqh656418510 <656418510@qq.com>
2026-09-23 19:42:09 +0800

    Update files

--- c++-study-4/day07/demo05/main4.cpp
+++ c++-study-4/day07/demo05/main4.cpp
@@ -11,5 +11,15 @@
 int main() {
     std::vector<int> numbers = {69, 23, 35, 13, 56};
 
+    // 排序（默认升序排序，即从小到大排序）
+    std::sort(numbers.begin(), numbers.end());
+
+    // 或者降序排序（即从大到小排序）
+    // std::sort(numbers.begin(), numbers.end(), std::greater<int>());
+
+    for (const int& number : numbers) {
+        std::cout << number << " ";
+    }
+
     return 0;
 }
