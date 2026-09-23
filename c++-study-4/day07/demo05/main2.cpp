9241cfce9997a7f86abacfd9e85fb49b8f6ad3a5
rqh656418510 <656418510@qq.com>
2026-09-23 19:34:54 +0800

    Update files

--- c++-study-4/day07/demo05/main2.cpp
+++ c++-study-4/day07/demo05/main2.cpp
@@ -13,10 +13,11 @@ int main() {
 
     std::vector<int> numbers = {69, 23, 35, 13, 56};
 
+    // 查找第一个元素 35
     std::vector<int>::iterator find_iter = std::find(numbers.begin(), numbers.end(), target_number);
 
     if (find_iter != numbers.end()) {
-        std::cout << "found number " << target_number << std::endl;
+        std::cout << "found number " << *find_iter << std::endl;
     } else {
         std::cout << "not found number " << target_number << std::endl;
     }
--- c++-study-4/day07/demo05/main3.cpp
+++ c++-study-4/day07/demo05/main3.cpp
@@ -11,5 +11,14 @@
 int main() {
     std::vector<int> numbers = {69, 23, 35, 13, 56};
 
+    // 查找第一个大于 15 的元素，第三个参数是可调用对象（如普通函数、函数对象、Lambda 表达式）
+    std::vector<int>::iterator find_iter = std::find_if(numbers.begin(), numbers.end(), [](const int& number) { return number > 15; });
+
+    if (find_iter != numbers.end()) {
+        std::cout << "found number > 15, it is " << *find_iter << std::endl;
+    } else {
+        std::cout << "not found number > 15" << std::endl;
+    }
+
     return 0;
 }
