6dfa758de09516ae0a819e6ab21f6eb84af5f642
rqh656418510 <656418510@qq.com>
2026-09-23 14:24:58 +0800

    Update files

--- c++-study-4/day07/demo04/main.cpp
+++ c++-study-4/day07/demo04/main.cpp
@@ -4,6 +4,58 @@
  * (a) 迭代器的使用
  */
 
+#include <iostream>
+#include <vector>
+
+void fun1() {
+    std::vector<int> v = {1, 2, 3};
+
+    // 可读写正向迭代器
+    for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+        // 可以更改元素值
+        *it = *it * 2;
+        std::cout << *it << std::endl;
+    }
+}
+
+void fun2() {
+    std::vector<int> v = {1, 2, 3};
+
+    // 只读正向迭代器
+    for (std::vector<int>::const_iterator it = v.begin(); it != v.end(); ++it) {
+        // 不可以更改元素值
+        std::cout << *it << std::endl;
+    }
+}
+
+void fun3() {
+    std::vector<int> v = {1, 2, 3};
+
+    // 可读写反向迭代器
+    for (std::vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
+        // 可以更改元素值
+        *it = *it * 2;
+        std::cout << *it << std::endl;
+    }
+}
+
+void fun4() {
+    std::vector<int> v = {1, 2, 3};
+
+    // 只读反向迭代器
+    for (std::vector<int>::const_reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
+        // 不可以更改元素值
+        std::cout << *it << std::endl;
+    }
+}
+
 int main() {
+    fun1();
+    std::cout << "--------------------------" << std::endl;
+    fun2();
+    std::cout << "--------------------------" << std::endl;
+    fun3();
+    std::cout << "--------------------------" << std::endl;
+    fun4();
     return 0;
 }
