32cba9523dca5a5483043888a9b7ee56a0bfa116
rqh656418510 <656418510@qq.com>
2026-09-23 20:00:09 +0800

    Update files

--- c++-study-4/day07/demo05/main4.cpp
+++ c++-study-4/day07/demo05/main4.cpp
@@ -14,7 +14,7 @@ int main() {
     // 排序（默认升序排序，即从小到大排序）
     std::sort(numbers.begin(), numbers.end());
 
-    // 或者降序排序（即从大到小排序）
+    // 或者降序排序（即从大到小排序），第三个参数是可调用对象（如普通函数、函数对象、Lambda 表达式）
     // std::sort(numbers.begin(), numbers.end(), std::greater<int>());
 
     for (const int& number : numbers) {
