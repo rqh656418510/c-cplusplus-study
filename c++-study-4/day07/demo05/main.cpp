0e5c5852d206e3a3f388ce828b5d67b1efd2168e
rqh656418510 <656418510@qq.com>
2026-09-23 19:10:31 +0800

    Update files

--- c++-study-4/day07/demo05/main.cpp
+++ c++-study-4/day07/demo05/main.cpp
@@ -1,7 +1,7 @@
 /**
  * 算法概述、内部处理、使用范例
  *
- * (a) 算法的使用
+ * (a) for_each 算法的使用
  */
 
 #include <algorithm>
@@ -9,15 +9,10 @@
 #include <vector>
 
 int main() {
-    std::vector<int> numbers = {69, 23, 35, 13, 5};
-    std::vector<int>::iterator iter_begin = numbers.begin();
-    std::vector<int>::iterator iter_end = numbers.end();
+    std::vector<int> numbers = {69, 23, 35, 13, 56};
 
-    std::sort(iter_begin, iter_end);
-
-    for (const auto &num : numbers) {
-        std::cout << num << std::endl;
-    }
+    // 第三个参数是可调用对象（如普通函数、函数对象、Lambda 表达式）
+    std::for_each(numbers.begin(), numbers.end(), [](const int number) { std::cout << number << " "; });
 
     return 0;
 }
---
+++ c++-study-4/day07/demo05/main3.cpp
@@ -0,0 +1,15 @@
+/**
+ * 算法概述、内部处理、使用范例
+ *
+ * (c) find_if 算法的使用
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers = {69, 23, 35, 13, 56};
+
+    return 0;
+}
